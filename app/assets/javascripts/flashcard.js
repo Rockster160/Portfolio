e26b14d352811c581e4edb33486944cb68275fa9
Rocco Nicholls <rocco11nicholls@gmail.com>
2014-11-26 18:28:55 -0700

    Got the max length working

--- app/assets/javascripts/flashcard.js
+++ app/assets/javascripts/flashcard.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
+
   $(".center-btn").click(function() {
-    $(this).siblings().toggleClass("center");
-  })
+    c = $(this).siblings("#flashcard-line");
+    c.toggleClass("center");
+  });
+
+  document.onkeyup = function () {
+    focused = document.activeElement.className;
+    if (focused == "flashcard-class" || focused == "flashcard-class center") {
+      var width = textWidth($(':focus').val(), "Comic Sans MS");
+      if (width > 300) {
+        str = $(':focus').val().split("");
+        str.pop();
+        $(':focus').val(str.join(""));
+      }
+    }
+  };
+
+  function textWidth(text, font) {
+    if (!textWidth.fakeEl) textWidth.fakeEl = $('<span class="hidden">').appendTo(document.body);
+    var htmlText = text || this.val() || this.text();
+    htmlText = textWidth.fakeEl.text(htmlText).html(); //encode to Html
+    htmlText = htmlText.replace(/\s/g, "&nbsp;"); //replace trailing and leading spaces
+    textWidth.fakeEl.html(htmlText).css('font', font || this.css('font'));
+    width = textWidth.fakeEl.width();
+    return width;
+  };
 })
