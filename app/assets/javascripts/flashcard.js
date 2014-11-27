2504d64839c041b796b10deb8282d78631e0275c
Rocco Nicholls <rocco11nicholls@gmail.com>
2014-11-26 17:07:27 -0700

    Removed log. Don't need the html. DO need to remember whether centered or not...

--- app/assets/javascripts/flashcard.js
+++ app/assets/javascripts/flashcard.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $(".center-button").click(function() {
     $(this).siblings().toggleClass("center");
-    console.log($(this).siblings("#flashcard-line").html());
   })
 })
