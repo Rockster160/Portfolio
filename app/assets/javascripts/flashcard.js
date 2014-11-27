a8b8a60d620e63d402955fecabf1ff37c3bc2b82
Rocco Nicholls <rocco11nicholls@gmail.com>
2014-11-26 17:02:37 -0700

    Trying to log the text in the space.

--- app/assets/javascripts/flashcard.js
+++ app/assets/javascripts/flashcard.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".center-button").click(function() {
     $(this).siblings().toggleClass("center");
-    console.log($(this).siblings().html());
+    console.log($(this).siblings("#flashcard-line").html());
   })
 })
