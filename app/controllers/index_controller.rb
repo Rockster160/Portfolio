ae50fb507104ad162b37d96e0fe251ee10568306
Rocco Nicholls <rocco11nicholls@gmail.com>
2014-12-06 11:45:50 -0700

    Put the lines into their own partial. Discovered new bug: Buttons don't update, so they always interpret the first card as the only card shown.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,54 +5,54 @@ class IndexController < ApplicationController
   def play
     @read = true
     @cards = FlashCard.all
-    if params[:pass_id]
-      @card = FlashCard.find(params[:pass_id].to_i)
-    else
-      @card = FlashCard.find(1)
-    end
-    @card_num = FlashCard.all.index(@card)
+    @card = FlashCard.find(1)
+    @card_num = FlashCard.all.index(@card) + 1
   end
 
   def flashcard
     all = FlashCard.all
-    old = FlashCard.all.index(FlashCard.find(params[:old].to_i))
+    old_flashcard = FlashCard.find(params[:old].to_i)
+    old_index = FlashCard.all.index(old_flashcard)
     case params[:type]
     when "new"
       @card = FlashCard.new
       @card.save
       @read = false
     when "edit"
-      @card = FlashCard.find(params[:old].to_i)
+      @card = old_flashcard
       @read = false
     when "next"
-      if old == all.length - 1
+      if old_index == all.length - 1
         back = 0
       else
-        back = old + 1
+        back = old_index + 1
       end
+      binding.pry
       @card = all[back]
       @read = true
     when "back"
-      if old == 0
+      if old_index == 0
         back = all.length - 1
       else
-        back = old - 1
+        back = old_index - 1
       end
       @card = all[back]
       @read = true
     when "save"
-      FlashCard.find(params[:old].to_i).save
-      @card = FlashCard.find(params[:old].to_i)
+      old_flashcard.save
+      @card = old_flashcard
       @read = true
     when "delete"
-      FlashCard.find(params[:old].to_i).destroy
+      old_flashcard.destroy
       @card = FlashCard.all.last
-      @read = true
     else
       @card = FlashCard.find(0)
       @read = true
     end
-      @card_num = FlashCard.all.index(@card)
+    if params[:status]
+      @read = params[:status]
+    end
+    @card_num = FlashCard.all.index(@card) + 1
     respond_to do |format|
       format.html
       format.js
