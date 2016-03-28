2e61130f8869cd39bd54cd6249e2e855946667ef
Rocco <rocco11nicholls@gmail.com>
2016-03-28 14:50:27 -0600

    Fixed the timing on the cron job

--- config/initializers/sidekiq_cron.rb
+++ config/initializers/sidekiq_cron.rb
@@ -1,4 +1,5 @@
 # ::Sidekiq::Cron::Job.destroy_all!
 # ::Sidekiq::Cron::Job.all/count
-::Sidekiq::Cron::Job.create(name: 'LitterReminder', cron: '*/2 * * * *', class: 'LitterReminderWorker')
-# ::Sidekiq::Cron::Job.create(name: 'LitterReminder', cron: '0 8 * * *', class: 'LitterReminderWorker')
+if Rails.env.production?
+  ::Sidekiq::Cron::Job.create(name: 'LitterReminder', cron: '0 8 * * *', class: 'LitterReminderWorker')
+end
