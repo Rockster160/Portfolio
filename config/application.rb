8467b259adeae07c5d64e773a032b7f8278abaac
Rocco <rocco11nicholls@gmail.com>
2016-03-29 20:14:32 -0600

    Fixed the time.

--- config/application.rb
+++ config/application.rb
@@ -13,10 +13,11 @@ module Portfolio
     # -- all .rb files in that directory are automatically loaded.
 
     config.secret_key_base = '9066475dd7ba28f4609cdf0e6df34d97216ef815207da74518a2da39f9a9d816a0463f555007fa99eb8f8ac2c875f23d370c2db41387cbfd621e6dca77b19ba4'
-
+    
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Mountain Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
