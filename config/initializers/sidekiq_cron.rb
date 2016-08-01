a4cdb871c182f9e6cda0cb7e9896e849b28f8221
Rocco <rocco11nicholls@gmail.com>
2016-08-01 12:31:41 -0600

    Stop scheduling the walker worker

--- config/initializers/sidekiq_cron.rb
+++ config/initializers/sidekiq_cron.rb
@@ -2,5 +2,5 @@
 # ::Sidekiq::Cron::Job.all/count
 if Rails.env.production?
   ::Sidekiq::Cron::Job.create(name: 'LitterReminder', cron: '0 23 * * *', class: 'LitterReminderWorker')
-  ::Sidekiq::Cron::Job.create(name: 'PokemonWalkerWorker', cron: '*/5 * * * *', class: 'PokemonWalkerWorker')
+  # ::Sidekiq::Cron::Job.create(name: 'PokemonWalkerWorker', cron: '*/5 * * * *', class: 'PokemonWalkerWorker')
 end
