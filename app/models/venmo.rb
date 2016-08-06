8d7cc9dc38d287f67af3e55a67932944f27c6c83
Rocco <rocco11nicholls@gmail.com>
2016-08-06 08:49:15 -0600

    Updated Venmo Charge time, send myself a message when trying to charge.

--- app/models/venmo.rb
+++ app/models/venmo.rb
@@ -19,6 +19,7 @@ class Venmo < ActiveRecord::Base
   end
 
   def charge(to, amount, note)
+    SmsWorker.perform_async('3852599640', "Charging #{to}")
     refresh_access_token if expired?
     response = HTTParty.post("https://api.venmo.com/v1/payments", body: {
       "access_token" => access_token,
@@ -26,6 +27,9 @@ class Venmo < ActiveRecord::Base
       "note" => note,
       "amount" => amount
     })
+    if response['error'].present?
+      SmsWorker.perform_async('3852599640', "Venmo Error: #{response['error']['message']}")
+    end
   end
 
   def expired?
