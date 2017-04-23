9407d8223b523b9d80c70881cb2f1ad98d814fbb
Rocco <rocco11nicholls@gmail.com>
2017-04-22 23:02:00 -0600

    Updated the timing for Cron

--- config/initializers/sidekiq_cron.rb
+++ config/initializers/sidekiq_cron.rb
@@ -4,6 +4,6 @@
 # ::Sidekiq::Cron::Job.destroy_all!
 # ::Sidekiq::Cron::Job.all/count
 if Rails.env.production?
-  ::Sidekiq::Cron::Job.create(name: 'LitterReminder', cron: '0 23 * * *', class: 'LitterReminderWorker')
-  ::Sidekiq::Cron::Job.create(name: 'ChargeBrothersWorker', cron: '0 14 5 * *', class: 'ChargeBrothersWorker')
+  ::Sidekiq::Cron::Job.create(name: 'LitterReminder', cron: '0 21 * * *', class: 'LitterReminderWorker')
+  ::Sidekiq::Cron::Job.create(name: 'ChargeBrothersWorker', cron: '0 11 5 * *', class: 'ChargeBrothersWorker')
 end
