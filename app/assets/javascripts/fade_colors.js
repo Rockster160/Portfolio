5a8d0956e3ecfb0a17509ab33c0c5f7c86b4f71d
Rocco <rocco11nicholls@gmail.com>
2017-05-04 18:54:11 -0600

    Added random colors and a color picker

--- app/assets/javascripts/fade_colors.js
+++ app/assets/javascripts/fade_colors.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
 
+  $('.colorpicker').spectrum({
+    change: function(spectrumColor) {
+      $(this).parents(".form-field").find(".color-field").val(spectrumColor.toHexString().toUpperCase());
+    }
+  })
+
+  $('.color-field').blur(function() {
+    $(this).parents(".form-field").find(".colorpicker").spectrum("set", $(this).val());
+  })
+
+  $('.random-color').click(function(evt) {
+    evt.preventDefault();
+    var rand_str = ["#"], possible = "0123456789ABCDEF"
+    for (var i=0; i<6; i++) {
+      var chosen = possible.charAt(Math.floor(Math.random() * possible.length));
+      rand_str.push(chosen);
+    }
+    $(this).parents('.form-field').children(".color-field").val(rand_str.join(""));
+    $("#color-form").submit();
+    return false;
+  })
+
   $("#color-form").submit(function(evt) {
     evt.preventDefault();
     $.get($(this).attr("action"), $(this).serialize()).success(function(data) {
@@ -14,6 +36,7 @@ $(document).ready(function() {
       $(this).append($("<div>", { class: "color-preview", style: "background-color: " + $(this).text().trim() }));
     })
   }
+  $('.color-field').blur()
   generateColorPreviews();
 
 })
