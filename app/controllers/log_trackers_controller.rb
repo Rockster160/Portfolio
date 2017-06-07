1af925e89fefe5d4df673a43591ec9c43f153eaa
Rocco Nicholls <rocco11nicholls@gmail.com>
2017-06-06 20:31:57 -0600

    Removed scopes screwing things up

--- app/controllers/log_trackers_controller.rb
+++ app/controllers/log_trackers_controller.rb
@@ -2,8 +2,9 @@ class LogTrackersController < ApplicationController
   before_action :authorize_admin
 
   def index
-    @loggers = LogTracker.not_me.not_log_tracker.order(created_at: :desc).page(params[:page])
-    @loggers = @loggers.by_fuzzy_url(params[:fuzzy_url]) if params[:fuzzy_url].present?
+    @loggers = LogTracker.order(created_at: :desc).page(params[:page])
+    # @loggers = LogTracker.not_me.not_log_tracker.order(created_at: :desc).page(params[:page])
+    # @loggers = @loggers.by_fuzzy_url(params[:fuzzy_url]) if params[:fuzzy_url].present?
   end
 
   def show
