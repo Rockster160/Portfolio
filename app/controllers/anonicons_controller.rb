80e6bffd3508207e90bdcd47c42b4b0e6188c046
Rocco Nicholls <rocco11nicholls@gmail.com>
2017-06-17 23:36:29 -0600

    Render inline with index as partial instead of separate template

--- app/controllers/anonicons_controller.rb
+++ app/controllers/anonicons_controller.rb
@@ -2,11 +2,11 @@ class AnoniconsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def index
-    @anonicon = Anonicon.generate(request.ip)
-  end
-
-  def show
     @anonicon = Anonicon.generate(params[:anon_str] || request.ip)
+
+    if request.xhr?
+      render partial: "show"
+    end
   end
 
 end
