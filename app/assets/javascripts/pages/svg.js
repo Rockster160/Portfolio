da4856439526f70376744eec52edfc8e32b46738
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-01-25 23:04:58 -0700

    SVG is more or less functional

--- app/assets/javascripts/pages/svg.js
+++ app/assets/javascripts/pages/svg.js
@@ -9,15 +9,23 @@ $(".ctr-svg_editors").ready(function() {
     var enteredText = this.textContent || this.innerText,
       spaceFix = enteredText.replace(/\s/g, " "),
       noJS = spaceFix.replace(/<script/g, "")
+    // $("#svg-raw").html($("#svg-raw").html().replace(/^([^\<]*?)\n/g, "<div>$1</div>"))
     SVG.raw = spaceFix
     $("#svg-box").html(SVG.raw)
   })
+
+  $(document).on("click", "#svg-box path", function() {
+    console.log(this);
+    // Display the control points for these
+  })
+
   // TODO: try/catch parsing errors, and instead display an error on screen
   // TODO: Toggle text wrap
   // TODO: Prettify input
 })
 
 window.onbeforeunload = function() {
+  $("#svg-box").html($("#svg-box").html().replace(/\<div\>/g, "").replace(/\<\/div\>/g, "\n"))
   var inputElement = document.getElementById("svg-raw")
   var enteredText = (inputElement.textContent || inputElement.innerText)
   if (enteredText !== null && enteredText.length > 0) { localStorage.setItem("svg", enteredText) }
