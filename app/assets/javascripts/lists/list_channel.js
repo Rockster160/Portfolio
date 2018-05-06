541934e8a282dffad50a3fc6f280907046bc0eee
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-05-05 21:16:32 -0600

    Fixed a bunch of list issues

--- app/assets/javascripts/lists/list_channel.js
+++ app/assets/javascripts/lists/list_channel.js
@@ -31,6 +31,7 @@ $(".ctr-lists.act-show").ready(function() {
         if (item_id == undefined || item_id.length == 0) { return false }
 
         var matching_items = $('.list-items .list-item-container[data-item-id=' + item_id + ']')
+        // Add Config icons
         if ($(this).find(".list-item-config .important").length > 0) {
           if (matching_items.find(".list-item-config .important").length == 0) { matching_items.find(".list-item-config").append($("<div>", {class: "important"})) }
         } else {
@@ -46,27 +47,34 @@ $(".ctr-lists.act-show").ready(function() {
         } else {
           matching_items.find(".list-item-config .recurring").remove()
         }
+        // Update Category of existing item
         var new_category = $(this).find(".list-item-config .category").text() || ""
-        matching_items.find(".list-item-config .category").text(new_category) // Update Category of existing item
-        matching_items.attr("data-sort-order", $(this).attr("data-sort-order")) // Update sort order of already found item
+        matching_items.find(".list-item-config .category").text(new_category)
+        // Update sort order of already found item
+        matching_items.attr("data-sort-order", $(this).attr("data-sort-order"))
+        // Update name correctly
         matching_items.find(".item-name").html($(this).find(".item-name").text())
-        matching_items.find(".list-item-checkbox").prop("checked", $(this).find(".list-item-checkbox").prop("checked"))
+
+        // Define whether it's checked or not - Only update if not locked
+        if ($(this).find(".list-item-config .locked").length == 0) {
+          matching_items.find(".list-item-checkbox").prop("checked", $(this).find(".list-item-checkbox").prop("checked"))
+        }
         return matching_items.length == 0
       })
-      $('.list-items').append(new_items)
+      $(".list-items").append(new_items)
 
-      // $(".list-items .list-item-container").each(function() {
-      //   var current_item = $(this)
-      //   var item_id = current_item.attr("data-item-id")
-      //   if ($(this).find(".list-item-config .locked").length > 0) { return }
-      //   if (updated_list_ids.toArray().includes(item_id)) {
-      //     // Item exists, uncheck to show it's not deleted
-      //     $(".list-item-container[data-item-id=" + item_id + "] input[type=checkbox]").prop("checked", false)
-      //   } else {
-      //     // Item does not exist, check to show it's deleted.
-      //     $(".list-item-container[data-item-id=" + item_id + "] input[type=checkbox]").prop("checked", true)
-      //   }
-      // })
+      $(".list-items .list-item-container").each(function() {
+        var current_item = $(this)
+        var item_id = current_item.attr("data-item-id")
+        if ($(this).find(".list-item-config .locked").length > 0) { return }
+        if (updated_list_ids.toArray().includes(item_id)) {
+          // Item exists, uncheck to show it's not deleted
+          $(".list-item-container[data-item-id=" + item_id + "] input[type=checkbox]").prop("checked", false)
+        } else {
+          // Item does not exist, check to show it's deleted.
+          $(".list-item-container[data-item-id=" + item_id + "] input[type=checkbox]").prop("checked", true)
+        }
+      })
 
       setImportantItems()
       reorderList()
