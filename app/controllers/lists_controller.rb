09a5c11290e90aae403ec73da976f2f7bea5a99f
Rocco Nicholls <rocco11nicholls@gmail.com>
2020-12-22 09:01:04 -0700

    Fixed nil

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -76,8 +76,8 @@ class ListsController < ApplicationController
   def receive_update
     @list = List.find(params[:id])
 
-    new_order = params[:list_item_order].reverse || []
-    new_order.each_with_index do |list_item_id, idx|
+    new_order = params[:list_item_order] || []
+    new_order.reverse.each_with_index do |list_item_id, idx|
       list_item = @list.list_items.with_deleted.find_by(id: list_item_id)
       next unless list_item.present?
       list_item.update(sort_order: idx, do_not_broadcast: true)
