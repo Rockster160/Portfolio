5d0766afbe069568d024888aa986c9a63a264f2b
Rocco Nicholls <rocco11nicholls@gmail.com>
2021-01-28 12:36:07 -0700

    Allow API usage of functions

--- app/controllers/functions_controller.rb
+++ app/controllers/functions_controller.rb
@@ -1,5 +1,6 @@
 class FunctionsController < ApplicationController
   before_action :authorize_admin
+  skip_before_action :verify_authenticity_token
 
   def index
     @functions = Function.order(id: :desc)
