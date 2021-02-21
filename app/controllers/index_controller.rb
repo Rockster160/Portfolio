35056dd4912dd97abf95138bde23c182d57cef62
Rocco Nicholls <rocco11nicholls@gmail.com>
2021-02-20 23:38:03 -0700

    View cleanup

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,15 +2,16 @@ class IndexController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def talk
+    from_number = params["From"]
     body = params["Body"]
-    from_user = current_user || User.find_by(phone: params["From"].gsub(/[^0-9]/, "").last(10))
+    from_user = current_user || User.find_by(phone: from_number.gsub(/[^0-9]/, "").last(10))
 
     if from_user.present?
-      res = CommandControl.parse(params[:message])
+      res = CommandControl.parse(body)
 
-      SmsWorker.perform_async(params["From"], res)
+      SmsWorker.perform_async(from_number, res)
     else
-      SmsWorker.perform_async(params["From"], "Sorry- I'm not sure who you are. Please log in and add your phone number before using SMS.")
+      SmsWorker.perform_async(from_number, "Sorry- I'm not sure who you are. Please log in and add your phone number before using SMS.")
     end
   end
 end
