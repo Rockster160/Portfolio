303d0625cb6509c77254e095a09bc3dbc73dc14e
Rocco Nicholls <rocco11nicholls@gmail.com>
2021-03-30 10:56:53 -0600

    Remove

--- app/controllers/functions_controller.rb
+++ app/controllers/functions_controller.rb
@@ -3,7 +3,6 @@ class FunctionsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    # Test comment
     @functions = Function.order(id: :desc)
   end
 
