877e739abb3faf22f7476e746621760558802d82
Rocco Nicholls <rocco11nicholls@gmail.com>
2021-08-15 17:48:05 -0600

    Ability to remove buckets

--- app/assets/javascripts/pages/buckets.js
+++ app/assets/javascripts/pages/buckets.js
@@ -5,6 +5,16 @@ $(".ctr-money_buckets").ready(function() {
     var clone = template.content.cloneNode(true)
 
     $(".buckets").append(clone)
+
+    if ($("input[type=checkbox]:checked").length == 0) {
+      $("input[type=checkbox]").first().prop("checked", true)
+    }
+  })
+
+  $(document).on("click", ".bucket-remove", function() {
+    if (confirm("Are you sure you want to delete?")) {
+      $(this).parents(".bucket").remove()
+    }
   })
 
   $(".buckets").sortable({
