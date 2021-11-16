c7e218fc0dbb2650b6316d627bc77d2e8fbee309
Rocco Nicholls <rocco11nicholls@gmail.com>
2021-11-16 09:01:51 -0700

    Can now make changes to scores

--- app/assets/javascripts/pages/bowling.js
+++ app/assets/javascripts/pages/bowling.js
@@ -1,5 +1,7 @@
+// 65 unique combinations of shots per frame
+// 4300 possible games (65*65 + 10th frame weirdness)
 $(".ctr-bowlings.act-show").ready(function() {
-  $(".throw").click(function() {
+  $(".shot").click(function() {
     moveToThrow($(this))
   })
 
@@ -7,47 +9,51 @@ $(".ctr-bowlings.act-show").ready(function() {
     addScore($(this).text())
   })
 
-  function collectThrows(player) {
-    return $(player).children(".bowling-cell.frame").map(function() {
-      return [$(this).children(".throw").text().split("")]
-    })
+  function collectThrows(bowler) {
+    return $(bowler).children(".bowling-cell.frame").map(function() {
+      return [$(this).children(".shot").map(function() {
+        var score = $(this).attr("data-score")
+        score = score == "" ? null : parseInt(score)
+        score = isNaN(score) ? null : score
+        return score
+      }).toArray()]
+    }).toArray()
   }
 
   function sumScores(arr) {
     return arr.reduce(function(a, b) {
-      return textToScore(a) + textToScore(b)
+      return parseInt(a) + parseInt(b)
     }, 0)
   }
 
   function calcScores() {
-    $(".bowling-table").each(function() {
-      var player = $(this)
-      var throws = collectThrows(player)
-      var frames = player.children(".bowling-cell.frame")
+    $(".bowling-table.bowler").each(function() {
+      var bowler = $(this)
+      var shots = collectThrows(bowler)
+      var frames = bowler.children(".bowling-cell.frame")
       var running_total = 0
       var still_calc = true
-      var max_score = 0
-      var current_frame = currentFrame()
+      // var max_score = 0
 
-      throws.each(function(idx) {
+      shots.forEach(function(tosses, idx) {
+        if (!still_calc) { return }
         var frame_total = 0
-        var tosses = this
         var first = tosses[0]
         var second = tosses[1]
 
         if (!first) { return still_calc = false }
-        if (current_frame == 10 && idx == 9) {
+        if (idx == 9) { // Tenth frame
           frame_total = sumScores(tosses)
-        } else if (first == "X") {
-          var next_tosses = throws[idx + 1] || []
-          if (next_tosses[0] == "X") {
-            var more_tosses = throws[idx + 2] || []
+        } else if (first == 10) {
+          var next_tosses = shots[idx + 1]
+          if (next_tosses[0] == 10) {
+            var more_tosses = shots[idx + 2] || []
             next_tosses = next_tosses.concat(more_tosses)
           }
           if (next_tosses.length < 2) { return still_calc = false }
           frame_total = 10 + sumScores(next_tosses.slice(0, 2))
-        } else if (second == "/") {
-          var next_tosses = throws[idx + 1] || []
+        } else if (first + second == 10) {
+          var next_tosses = shots[idx + 1] || []
           if (next_tosses.length < 1) { return still_calc = false }
           frame_total = 10 + sumScores(next_tosses.slice(0, 1))
         } else {
@@ -56,43 +62,106 @@ $(".ctr-bowlings.act-show").ready(function() {
 
         running_total += frame_total
         var score_holder = $(frames[idx]).children(".score")
-        score_holder.attr("data-frame-score", frame_total)
         score_holder.text(running_total)
       })
     })
+  }
+
+  function recalculateFrame(toss) {
+    var frame = toss.parent(".frame")
+    var shots = frame.children(".shot")
+    var shot_idx = shots.index(toss)
+    var toss_score = shotScore(toss)
+
+    var prev_shot = $(shots[shot_idx - 1])
+    var prev_text = prev_shot.length > 0 ? prev_shot.text() : null
+    var prev_val = shotScore(prev_shot)
+
+    var next_shot = $(shots[shot_idx + 1])
+    var next_text = next_shot.length > 0 ? next_shot.text() : null
+
+    var actual_val = scoreToVal(toss.text(), !prev_shot ? 0 : prev_val)
+
+    toss.attr("data-score", actual_val)
+
+    if (toss.text() == "X") {
+      toss.parent(".frame").children(".shot").text("").attr("data-score", "")
+      toss.text("X").attr("data-score", 10)
+    } else if (next_text == "/") {
+      next_shot.text("/").attr("data-score", 10 - shotScore(toss))
+    }
+
+    // if (frame_num == "10") {
+    // } else if (shot_idx == 0 && actual_val == 10) {
+    //   toss.parent(".frame").children(".shot").text("").attr("data-score", "")
+    //   toss.text("X").attr("data-score", 10)
+    // } else if (shot_idx == 1 && prev_val + actual_val >= 10) {
+    //   toss.text("/").attr("data-score", actual_val)
+    // } else {
+    //   toss.text()
+    // }
+
+    // if (actual_val == 10) {
+    //   toss.text("X")
+    // }
   }
 
   function addScore(text) {
     if (currentFrame() == 10) { return addTenthFrameScore(text) }
 
     var score = textToScore(text)
-    var toss = $(".throw.current")
-    var throw_num = currentThrowNum()
+    var toss = $(".shot.current")
+    var prev_shot = $(toss.parent(".frame").children(".shot")[0])
+    var actual_val = scoreToVal(score, shotScore(prev_shot))
+    toss.attr("data-score", actual_val)
 
-    if (throw_num == 1) {
+    var shot_num = currentThrowNum()
+
+    if (shot_num == 1) {
       if (score >= 10) {
+        toss.parent(".frame").children(".shot").text("") // Clear second shot
         toss.text("X")
+        recalculateFrame(toss)
         moveToNextFrame()
       } else {
         toss.text(score == "0" ? "-" : score)
+        recalculateFrame(toss)
         moveToNextThrow()
       }
-    } else if (throw_num == 2) {
-      var prev_throw = $($(".throw.current").parent(".frame").children(".throw")[0])
-      // If first throw is blank, add score to first frame instead
-      if (prev_throw.text() == "") {
-        moveToThrow(prev_throw)
+    } else if (shot_num == 2) {
+      if (prev_shot.text() == "X") {
+        moveToNextFrame()
+      }
+      // If first shot is blank, add score to first frame instead
+      if (prev_shot.text() == "") {
+        moveToThrow(prev_shot)
         return addScore(score)
       }
 
-      if (score + throwScore(prev_throw) >= 10) {
+      if (score + shotScore(prev_shot) >= 10) {
         toss.text("/")
       } else {
         toss.text(score == "0" ? "-" : score)
       }
 
+      recalculateFrame(toss)
       moveToNextFrame()
     }
+  }
+
+  function minMax(a, b, c) {
+    return [a, b, c].sort(function(x, y) { return x - y })[1]
+  }
+
+  function scoreToVal(score, prev_score) {
+    prev_score = parseInt(prev_score) || 0
+    score = minMax(0, 10 - prev_score, textToScore(score))
+
+    if (!isNaN(score)) { return score }
+    if (score == "-") { return 0 }
+    if (score == "X") { return 10 }
+    if (score == "/") { return 10 - prev_score }
+    return 0
   }
 
   function textToScore(text) {
@@ -104,14 +173,22 @@ $(".ctr-bowlings.act-show").ready(function() {
   }
 
   function addTenthFrameScore(score) {
-    var throw_num = currentThrowNum()
-    var toss = $(".throw.current")
+    var shot_num = currentThrowNum()
+    var toss = $(".shot.current")
     var toss_score = textToScore(score)
-    var throws = toss.parent().children(".throw")
-    var prev_throw = $(throws[throw_num - 2])
-    var prev_score = throwScore(prev_throw)
+    var shots = toss.parent().children(".shot")
+    var prev_shot = $(shots[shot_num - 2])
+    var prev_score = shotScore(prev_shot)
+    var actual_val = 0
 
-    if (prev_throw.length > 0 && prev_score < 10 && prev_score + toss_score >= 10) {
+    if (prev_shot.text == "X" || prev_shot.text == "/") {
+      actual_val = scoreToVal(score, prev_score)
+    } else {
+      actual_val = scoreToVal(score, 0) // After a close in the 10th, acts as a new frame
+    }
+    toss.attr("data-score", actual_val)
+
+    if (prev_shot.length > 0 && prev_score < 10 && prev_score + toss_score >= 10) {
       toss.text("/")
       moveToNextThrow()
     } else if (toss_score >= 10) {
@@ -120,49 +197,49 @@ $(".ctr-bowlings.act-show").ready(function() {
     } else {
       toss.text(score == "0" ? "-" : score)
 
-      if (throw_num == 1 || prev_score >= 10) { return moveToNextThrow() }
+      if (shot_num == 1 || prev_score >= 10) { return moveToNextThrow() }
       moveToNextFrame()
     }
   }
 
   function currentFrame() {
-    var toss = $(".throw.current")
+    var toss = $(".shot.current")
     var frame = toss.parent()
 
     return parseInt(frame.attr("data-frame"))
   }
 
   function currentThrowNum() {
-    var toss = $(".throw.current")
+    var toss = $(".shot.current")
     var frame = toss.parent()
-    var throws = frame.children(".throw")
+    var shots = frame.children(".shot")
 
-    return throws.index(toss) + 1
+    return shots.index(toss) + 1
   }
 
   function moveToThrow(toss) {
-    $(".throw.current").removeClass("current")
+    $(".shot.current").removeClass("current")
     toss.addClass("current")
   }
 
   function moveToNextThrow() {
     calcScores()
-    var next_throw = $(".throw.current").next(".throw")
+    var next_shot = $(".shot.current").next(".shot")
 
-    if (next_throw.length > 0) {
-      moveToThrow(next_throw)
+    if (next_shot.length > 0) {
+      moveToThrow(next_shot)
     } else {
       moveToNextFrame()
     }
   }
 
   function gotoNextFrame() {
-    var next_frame = $(".throw.current").parent().next(".frame")
+    var next_frame = $(".shot.current").parent().next(".frame")
 
-    moveToThrow(next_frame.children(".throw").first())
+    moveToThrow(next_frame.children(".shot").first())
   }
 
-  function throwScore(toss) {
+  function shotScore(toss) {
     var score = $(toss).text()
 
     return textToScore(score)
@@ -170,15 +247,15 @@ $(".ctr-bowlings.act-show").ready(function() {
 
   function gotoNextPlayer() {
     calcScores()
-    $(".throw.current").removeClass("current")
+    $(".shot.current").removeClass("current")
   }
 
   function moveToNextFrame() {
     calcScores()
-    // If there are more players, should go to next player
-    if ($(".throw.current").parent().attr("data-frame") == "10") {
-      var frame_throws = $(".throw.current").parent(".frame").children(".throw")
-      if (throwScore(frame_throws[0]) + throwScore(frame_throws[1]) >= 10) {
+    // If there are more players, should go to next bowler
+    if ($(".shot.current").parent().attr("data-frame") == "10") {
+      var frame_shots = $(".shot.current").parent(".frame").children(".shot")
+      if (shotScore(frame_shots[0]) + shotScore(frame_shots[1]) >= 10) {
         if (currentThrowNum() < 3) { return moveToNextThrow() }
       }
 
