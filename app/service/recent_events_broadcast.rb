20c1955afa2d5bcba182f4f5a9102ff43dc2969b
Rocco Nicholls <rocco11nicholls@gmail.com>
2022-03-18 09:29:11 -0600

    Serialize more events

--- app/service/recent_events_broadcast.rb
+++ app/service/recent_events_broadcast.rb
@@ -1,6 +1,6 @@
 class RecentEventsBroadcast
   def self.call
-    events = ::ActionEvent.order(timestamp: :desc).limit(10)
+    events = ::ActionEvent.order(timestamp: :desc).limit(30)
 
     ActionCable.server.broadcast "recent_events_channel", recent_events: events.serialize
   end
