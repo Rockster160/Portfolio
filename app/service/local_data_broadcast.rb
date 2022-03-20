855415bda55be957fedf3a8d247155e0cfbab210
Rocco Nicholls <rocco11nicholls@gmail.com>
2022-03-19 20:44:23 -0600

    Calendar should always show today

--- app/service/local_data_broadcast.rb
+++ app/service/local_data_broadcast.rb
@@ -16,8 +16,23 @@ class LocalDataBroadcast
 
   def encriched_data(to_enrich)
     to_enrich.tap do |data|
+      data[:calendar] = enrich_calendar(data[:calendar]) if data.key?(:calendar)
       data[:reminders] = enrich_reminders(data[:reminders]) if data.key?(:reminders)
     end
+  end
+
+  def enrich_calendar(calendar_data)
+    today_str = Time.current.in_time_zone("Mountain Time (US & Canada)").strftime("%b %-d, %Y:")
+    return calendar_data if calendar_data.include?(today_str)
+
+    today_chunk = [
+      today_str,
+      "------------------------",
+      "  No Events left today",
+      "",
+    ]
+
+    today_chunk + calendar_data
   end
 
   def enrich_reminders(reminder_data)
@@ -29,7 +44,7 @@ class LocalDataBroadcast
       time_words = distance_of_time_in_words(time, now)
       future = time > now
       direction = future ? "from now" : "ago"
-      next if time > now + 1.day
+      next if time > now.end_of_day
       name = future ? reminder[:name] : "[color pink]#{reminder[:name]}[/color]"
       "#{name} [color #{future ? :grey : "#807A40"}]#{time_words} #{direction}[/color]"
     rescue StandardError => e
