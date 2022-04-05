babbf7388bc67bcd71b9e78eecf83fa98f4863e5
Rocco Nicholls <rocco11nicholls@gmail.com>
2022-04-04 21:25:29 -0600

    Updated command and sidekiq

--- config/initializers/1_sidekiq.rb
+++ config/initializers/1_sidekiq.rb
@@ -7,10 +7,10 @@ config = { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/0") }
 
 Sidekiq.configure_server do |c|
   c.redis = config
-  c.error_handlers << Proc.new do |exception, context_hash|
-    webhook = "https://hooks.slack.com/services/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"
-    ::Slack::Notifier.new(webhook, channel: "#portfolio", username: "Help-Bot").ping("Sidekiq Error: >>> #{exception}: #{context_hash}", attachments: [])
-  end
+  # c.error_handlers << Proc.new do |exception, context_hash|
+  #   webhook = "https://hooks.slack.com/services/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"
+  #   ::Slack::Notifier.new(webhook, channel: "#portfolio", username: "Help-Bot").ping("Sidekiq Error: >>> #{exception}: #{context_hash}", attachments: [])
+  # end
 end
 
 Sidekiq.configure_client { |c| c.redis = config }
