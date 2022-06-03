e52dcd050865354fb602871140e277ccf3aa83d8
Rocco Nicholls <rocco11nicholls@gmail.com>
2022-06-03 11:22:32 -0600

    Fixed overwrite

--- app/service/loadtime_broadcast.rb
+++ app/service/loadtime_broadcast.rb
@@ -3,9 +3,11 @@ module LoadtimeBroadcast
 
   def call(data=nil)
     if data.present?
+      file = JSON.parse(File.read("loadtime.json") || "{}")
+      data = file.merge(data)
       File.write("loadtime.json", data.to_json)
     else
-      data = JSON.parse(File.read("loadtime.json"))
+      JSON.parse(File.read("loadtime.json") || "{}")
     end
 
     ActionCable.server.broadcast "loadtime_channel", data.deep_symbolize_keys
