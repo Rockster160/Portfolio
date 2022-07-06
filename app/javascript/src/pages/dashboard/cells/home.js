6726a19c7a9e1e0f012f0c4a1a8e85e31d3ef1f4
Rocco Nicholls <rocco11nicholls@gmail.com>
2022-07-06 14:33:46 -0600

    Attempt to fix states

--- app/javascript/src/pages/dashboard/cells/home.js
+++ app/javascript/src/pages/dashboard/cells/home.js
@@ -85,7 +85,7 @@ import { dash_colors } from "../vars"
         console.log("Garage: ", JSON.stringify(msg))
 
         cell.data.garage = cell.data.garage || {}
-        cell.data.garage.open = msg.garageState == "open"
+        cell.data.garage.open = msg.data.garageState == "open"
 
         renderLines()
       })
