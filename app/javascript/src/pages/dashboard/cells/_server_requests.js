e7e7102bac68a4716661e5d6f3bb18717b97274d
Rocco Nicholls <rocco11nicholls@gmail.com>
2022-10-10 20:56:29 -0600

    Attempt to fix server issues

--- app/javascript/src/pages/dashboard/cells/_server_requests.js
+++ app/javascript/src/pages/dashboard/cells/_server_requests.js
@@ -2,81 +2,81 @@ import consumer from "./../../../channels/consumer"
 import { dash_colors } from "../vars"
 var demo = !true
 
-if (window.location.pathname == "/dashboard") {
-  // var cell = Cell.register({
-  //   title: "",
-  //   text: "",
-  //   commands: {},
-  //   refreshInterval: Time.minute(),
-  //   reloader: function(cell) {},
-  //   command: function(text, cell) {},
-  //   socket: {
-  //     url: "",
-  //     subscription: {
-  //       channel: "",
-  //       channel_id: "",
-  //     },
-  //     receive: function(cell, msg) {}
-  //   },
-  // })
+// var cell = Cell.register({
+//   title: "",
+//   text: "",
+//   commands: {},
+//   refreshInterval: Time.minute(),
+//   reloader: function(cell) {},
+//   command: function(text, cell) {},
+//   socket: {
+//     url: "",
+//     subscription: {
+//       channel: "",
+//       channel_id: "",
+//     },
+//     receive: function(cell, msg) {}
+//   },
+// })
 
-  Server = function() {}
-  Server.request = function(url, type, data) {
-    return $.ajax({
-      url: url,
-      data: data || {},
-      dataType: "text",
-      type: type || "GET",
-    })
-    // Catch fails and show a useful error
+Server = function() {}
+Server.request = function(url, type, data) {
+  return $.ajax({
+    url: url,
+    data: data || {},
+    dataType: "text",
+    type: type || "GET",
+  })
+  // Catch fails and show a useful error
+}
+Server.post   = function(url, data) { return Server.request(url, "POST",  data) }
+Server.patch  = function(url, data) { return Server.request(url, "PATCH", data) }
+Server.get    = function(url, data) { return Server.request(url, "GET",   data) }
+Server.socket = function(subscription, receive) {
+  var receive = receive
+  var ws_protocol = location.protocol == "https:" ? "wss" : "ws", ws_open = false
+  if (typeof subscription != "object") {
+    subscription = { channel: subscription }
   }
-  Server.post   = function(url, data) { return Server.request(url, "POST",  data) }
-  Server.patch  = function(url, data) { return Server.request(url, "PATCH", data) }
-  Server.get    = function(url, data) { return Server.request(url, "GET",   data) }
-  Server.socket = function(subscription, receive) {
-    var receive = receive
-    var ws_protocol = location.protocol == "https:" ? "wss" : "ws", ws_open = false
-    if (typeof subscription != "object") {
-      subscription = { channel: subscription }
-    }
 
-    return {
-      url: ws_protocol + "://" + location.host + "/cable",
-      authentication: function() {
-        var ws = this
-        ws.send({ subscribe: subscription })
-      },
-      presend: function(packet) {
-        if (typeof packet != "object" || !packet.subscribe) {
-          if (typeof packet == "string") {
-            let new_packet = {}
-            new_packet[packet] = null
-            packet = new_packet
-          }
-          packet = {
-            command: "message",
-            identifier: JSON.stringify(subscription),
-            data: JSON.stringify(packet)
-          }
-        } else {
-          packet = {
-            command: "subscribe",
-            identifier: JSON.stringify(packet.subscribe)
-          }
+  return {
+    url: ws_protocol + "://" + location.host + "/cable",
+    authentication: function() {
+      var ws = this
+      ws.send({ subscribe: subscription })
+    },
+    presend: function(packet) {
+      if (typeof packet != "object" || !packet.subscribe) {
+        if (typeof packet == "string") {
+          let new_packet = {}
+          new_packet[packet] = null
+          packet = new_packet
         }
-
-        return packet
-      },
-      receive: function(msg) {
-        var cell = this
-        var msg_data = JSON.parse(msg.data)
-        if (msg_data.type == "ping" || !msg_data.message) { return }
-
-        receive.call(cell, msg_data.message)
+        packet = {
+          command: "message",
+          identifier: JSON.stringify(subscription),
+          data: JSON.stringify(packet)
+        }
+      } else {
+        packet = {
+          command: "subscribe",
+          identifier: JSON.stringify(packet.subscribe)
+        }
       }
+
+      return packet
+    },
+    receive: function(msg) {
+      var cell = this
+      var msg_data = JSON.parse(msg.data)
+      if (msg_data.type == "ping" || !msg_data.message) { return }
+
+      receive.call(cell, msg_data.message)
     }
   }
+}
 
+if (window.location.pathname == "/dashboard") {
   // window.localDataChannel.request()
   window.localDataChannel = consumer.subscriptions.create({
     channel: "LocalDataChannel"
