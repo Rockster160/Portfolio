3f6a8c352b85070fc8aa3f0c20990c70b077b37d
Rocco Nicholls <rocco11nicholls@gmail.com>
2022-10-14 16:36:06 -0600

    Added projects cell

---
+++ app/javascript/src/pages/dashboard/cells/projects.js
@@ -0,0 +1,37 @@
+import { Text } from "../_text"
+
+(function() {
+  Cell.register({
+    title: "Projects",
+    text: "Loading...",
+    wrap: true,
+    socket: Server.socket({
+      channel: "ListJsonChannel",
+      channel_id: "list_163",
+    }, function(msg) {
+      if (!msg.list_data) { return }
+
+      var lines = Text.numberedList(msg.list_data.list_items)
+      this.text(lines.join("\n"))
+    }),
+    reloader: function() {
+      this.ws.send({ get: true })
+    },
+    command: function(text) {
+      let data = { message: text }
+      if (/^-\d+$/.test(text)) {
+        var num = parseInt(text.match(/\d+/)[0])
+        var lines = this.text().split("\n")
+        var item = lines[num-1]
+        data.remove = item.replace(/^\d+\. /, "")
+        delete data.message
+      }
+      if (!/^[-|+|add|remove]/gi.test(text)) {
+        data.add = text
+        delete data.message
+      }
+
+      this.ws.send(data)
+    },
+  })
+})()
