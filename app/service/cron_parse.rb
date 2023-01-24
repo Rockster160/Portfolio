ad05d6415b6bc8b860b4fa01a202830c727a5080
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-01-23 17:06:46 -0700

    Cron syntax supports multiple crons now

--- app/service/cron_parse.rb
+++ app/service/cron_parse.rb
@@ -4,7 +4,11 @@ module CronParse
   def next(cron)
     # current_user.timezone
     Time.use_zone("America/Denver") {
-      Time.zone.at(Fugit::Cron.parse(cron).next_time.to_i)
+      cron.split(/,\s*/).filter_map { |cron_str|
+        Fugit::Cron.parse(cron_str)&.next_time&.to_i&.then { |i| Time.zone.at(i) }
+      }.min
     }
+  rescue NoMethodError # parse returns `nil` if it fails, then `next_time` throws an error
+    nil
   end
 end
