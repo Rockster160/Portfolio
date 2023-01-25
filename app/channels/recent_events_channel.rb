2e037f10fe4dd36326c4167b4f13ae07c60772b5
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-01-24 19:39:28 -0700

    Allow pushing events through WS

--- app/channels/recent_events_channel.rb
+++ app/channels/recent_events_channel.rb
@@ -2,4 +2,13 @@ class RecentEventsChannel < ApplicationCable::Channel
   def subscribed
     stream_from "recent_events_channel"
   end
+
+  def receive(data)
+    data = data.deep_symbolize_keys!
+
+    event = current_user.action_events.create!(
+      data.slice(:event_name, :notes, :timestamp)
+    )
+    ActionEventBroadcastWorker.perform_async(event.id)
+  end
 end
