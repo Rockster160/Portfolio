5cb70d9e2604320441c641c0472e04de715aefef
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-02-09 10:50:34 -0700

    Updated BetterJson

--- app/service/better_json.rb
+++ app/service/better_json.rb
@@ -1,6 +1,10 @@
 # {hello: :world, a: [1, 2, 3], b: { c: [], d: {e: "f", g: :h, i: [{j: :k}, {L: [:m, 9, "o"]}]}}}.better
 # {}.better.pretty
-class BetterJson
+
+module FancyRenderJson
+  module_function
+
+  INDENT = "  ".freeze
   COLOR_MAP = {
     key: :yellow,
     string: :lime,
@@ -11,8 +15,47 @@ class BetterJson
     null: :grey,
     unknown: :red,
   }.freeze
-  INDENT = "  ".freeze
 
+  def display(hash)
+    puts pretty(hash)
+  end
+
+  def format(obj, depth=nil)
+    case obj
+    when String then "\"#{obj}\"".colorize(COLOR_MAP[:string])
+    when Symbol then ":#{obj}".colorize(COLOR_MAP[:symbol])
+    when TrueClass, FalseClass then obj.to_s.colorize(COLOR_MAP[:boolean])
+    when Numeric then obj.to_s.colorize(COLOR_MAP[:numeric])
+    when NilClass then "nil".colorize(COLOR_MAP[:null])
+    when Date, Time then obj.to_s.colorize(COLOR_MAP[:date])
+    when Array
+      "[#{obj.map { |v| format(v, depth) }.join(", ")}]"
+    when Hash, BetterJson
+      return pretty(obj, depth) unless depth.nil?
+
+      "{" + obj.map { |k,v|
+        "#{k.to_s.colorize(COLOR_MAP[:key])}: #{format(v)}"
+      }.join(", ") + "}"
+    else
+      "???<#{obj}|#{obj.class}>???".colorize(COLOR_MAP[:unknown])
+    end
+  end
+
+  def pretty(hash, depth=0)
+    curdent = INDENT*depth
+
+    [
+      "{",
+      *hash.map { |k, v| "#{INDENT}#{k.to_s.colorize(COLOR_MAP[:key])}: #{format(v, depth+1)}" },
+      "}"
+    ].join("\n#{curdent}")
+  end
+end
+
+class BetterJson
+  attr_accessor :hash
+
+  # ===== Hacky overwrite methods
   # Allow dot access for nested keys
   def method_missing(method, *args, &block)
     if @hash.key?(method.to_s.to_sym)
@@ -22,69 +65,46 @@ class BetterJson
     end
   end
 
+  # Quack like a class that inherits from Hash
+  def is_a?(klass)
+    klass == BetterJson || @hash.is_a?(klass)
+  end
+
+  # Use "key: val" syntax instead of ":key => val"
+  def to_s
+    inspect.uncolor
+  end
+
+  # Use "key: val" syntax instead of ":key => val" + colors!
+  def inspect
+    FancyRenderJson.display(@hash)
+  end
+
+  # Quack like a hash
+  delegate(
+    :to_json,
+    :as_json,
+    to: :hash
+  )
+  # / ===== Hacky overwrite methods
+
   def initialize(*args)
     @hash = HashWithIndifferentAccess.new(*args)
   end
 
-  def to_json
-    @hash.to_json
-  end
-
-  def colorit(obj, depth=nil)
-    case obj
-    when String then "\"#{obj}\"".colorize(COLOR_MAP[:string])
-    when Symbol then ":#{obj}".colorize(COLOR_MAP[:symbol])
-    when TrueClass, FalseClass then obj.to_s.colorize(COLOR_MAP[:boolean])
-    when Numeric then obj.to_s.colorize(COLOR_MAP[:numeric])
-    when NilClass then "nil".colorize(COLOR_MAP[:null])
-    when Date then v.to_s.colorize(COLOR_MAP[:date])
-    when Array
-      "[#{obj.map { |v| colorit(v, depth) }.join(", ")}]"
-    when Hash, BetterJson
-      return obj.to_pretty(depth) unless depth.nil?
-      "{" + obj.map { |k,v|
-        "#{k.to_s.colorize(COLOR_MAP[:key])}: #{colorit(v)}"
-      }.join(", ") + "}"
-    else
-      "???<#{obj}|#{obj.class}>???".colorize(COLOR_MAP[:unknown])
-    end
-  end
-
-  def to_s
-    inspect
-  end
-
-  def inspect
-    colorit(@hash)
-  end
-
-  def to_pretty(depth=0)
-    curdent = INDENT*depth
-
-    [
-      "{",
-      *@hash.map { |k, v| "#{INDENT}#{k.to_s.colorize(COLOR_MAP[:key])}: #{colorit(v, depth+1)}" },
-      "}"
-    ].join("\n#{curdent}")
-  end
-
-  def pretty(depth=0)
-    puts to_pretty
-  end
-
   # Break into dot keys to value
-  def branches(hash=nil)
-    hash ||= self
-    return hash unless hash.is_a?(Hash) || hash.is_a?(Array)
-    return hash if hash.is_a?(Hash) && hash.none? { |k, v| v.is_a?(Hash) }
+  def branches(branchobj=nil)
+    branchobj ||= self
+    return branchobj unless branchobj.is_a?(Hash) || branchobj.is_a?(Array)
+    return branchobj if branchobj.is_a?(Hash) && branchobj.none? { |k, v| v.is_a?(Hash) }
 
     # Hash with hashes
-    if hash.is_a?(Array)
-      hash = hash.each_with_index.with_object({}) do |(bval, idx), obj|
+    if branchobj.is_a?(Array)
+      branchobj = branchobj.each_with_index.with_object({}) do |(bval, idx), obj|
         obj["#{idx}"] = bval
       end
     end
-    hash.each_with_object({}) do |(k, v), obj|
+    branchobj.each_with_object({}) do |(k, v), obj|
       next obj[k.to_s] = v if v.nil? || v == false # Falsey things will act weird in #branches
 
       bdata = branches(v)
