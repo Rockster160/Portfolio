6124ff4661b05ec802e733d559b52ae55fe3abb6
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-02-21 09:21:10 -0700

    Properly use last result val instead of trying to pull off ctx which has no guarantee on order

--- app/service/jarvis/execute/custom.rb
+++ app/service/jarvis/execute/custom.rb
@@ -6,26 +6,25 @@ class Jarvis::Execute::Custom < Jarvis::Execute::Executor
     task_input_data = run_task.inputs.to_h
     data = run_task.inputs.map(&:first).zip(evalargs).map { |k, v|
       type = task_input_data[k].each { |d| break d[:return] if d.is_a?(Hash) && d.key?(:return) }
+      # If there is no `return` this should fail somehow?
       [k, ::Jarvis::Execute::Cast.cast(v, type)]
     }.to_h
 
     custom_ctx, custom_task, custom_data = ::Jarvis::Execute.call_with_data(
       run_task,
       {
-        ctx: { i: jil.ctx[:i] },
+        ctx: { i: jil.ctx[:i] }, # Pass `i` so that we properly error our over 1k tasks
         input_vars: data,
       }
     )
 
-    jil.ctx[:i] = run_task.last_ctx[:i]
+    jil.ctx[:i] = run_task.last_ctx[:i] # Take the `i` back from the other task to continue counting
 
     custom_error = custom_ctx[:msg].find { |msg| msg.include?("] Failed:") }
     if custom_error
       raise custom_error
     else
-      # Add last_result_val to JarvisTask and use that instead.
-      v = custom_ctx[:vars].to_a.last[1]
-      ::Jarvis::Execute::Cast.cast(v, run_task.output_type)
+      ::Jarvis::Execute::Cast.cast(custom_task.last_result_val, run_task.output_type)
     end
   end
 end
