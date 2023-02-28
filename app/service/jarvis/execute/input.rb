d7bcaede883922d839be97be3ebb495775bcbb93
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-02-28 07:54:05 -0700

    Safer error

--- app/service/jarvis/execute/input.rb
+++ app/service/jarvis/execute/input.rb
@@ -1,7 +1,7 @@
 class Jarvis::Execute::Input < Jarvis::Execute::Executor
   def method_missing(method, *method_args, &block)
     input_data = jil.data.dig(:input_vars)&.better
-    super(method, *method_args, &block) unless input_data&.key?(method)
+    return unless input_data&.key?(method)
 
     input_data[method]
   end
