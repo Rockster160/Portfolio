6e295bf52bce694cb6abf8fe917dd086fd38ea85
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-03-24 12:04:55 -0600

    Added on list item as well, also include list data

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -193,6 +193,8 @@ class ListItem < ApplicationRecord
 
     rendered_message = ListsController.render template: "list_items/index", locals: { list: self.list }, layout: false
     ActionCable.server.broadcast "list_#{self.list_id}_html_channel", { list_html: rendered_message, timestamp: Time.current.to_i }
+
+    JarvisTriggerWorker.perform_async(:list.to_s, { list_name: list.name, list_id: list.id }.to_json, { user: list.users.ids }.to_json)
   end
 
 end
