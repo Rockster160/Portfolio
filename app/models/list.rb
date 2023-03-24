01fb6b0af4d464b9056519f4e591e0e0af38e922
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-03-24 11:56:04 -0600

    Strings for sidekkkiq

--- app/models/list.rb
+++ app/models/list.rb
@@ -224,7 +224,7 @@ class List < ApplicationRecord
     rendered_message = ListsController.render template: "list_items/index", locals: { list: self }, layout: false
     ActionCable.server.broadcast "list_#{self.id}_html_channel", { list_html: rendered_message, timestamp: Time.current.to_i }
 
-    JarvisTriggerWorker.perform_async(:list, {}, { user: users.ids })
+    JarvisTriggerWorker.perform_async(:list.to_s, {}.to_json, { user: users.ids }.to_json)
   end
 
 end
