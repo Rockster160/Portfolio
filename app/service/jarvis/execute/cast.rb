288a600ec7823e9f06b154337f13487c6494c96a
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-03-24 17:16:35 -0600

    Force cast everything

--- app/service/jarvis/execute/cast.rb
+++ app/service/jarvis/execute/cast.rb
@@ -1,9 +1,16 @@
 class Jarvis::Execute::Cast < Jarvis::Execute::Executor
-  def self.cast(val, to)
+  def self.cast(val, to, force: false)
     return val if to.to_sym == :any
 
     if ::Jarvis::Execute::Raw::CASTABLE.include?(to.to_sym)
       return ::Jarvis::Execute::Raw.send(to.to_sym, val)
+    elsif force
+      case to.to_sym
+      when :array
+        return val.try(:to_a) || []
+      when :hash
+        return val.try(:to_h) || val.try(:to_json) || []
+      end
     end
 
     raise "Unknown type to cast: #{to.class}(#{to}): #{val}"
