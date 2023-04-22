e1e542f8f9a3964db8f2602da9bba1b8db29e32c
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-04-21 18:20:03 -0600

    Remove logging. Stupid cache

--- public/quick_actions.js
+++ public/quick_actions.js
@@ -1,6 +1,3 @@
 import { garage } from './quick_actions/garage.js';
 import { tesla } from './quick_actions/tesla.js';
 import { command } from './quick_actions/command.js';
-
-console.log("Done imports");
-console.log("cmd", command);
--- public/quick_actions/command.js
+++ public/quick_actions/command.js
@@ -2,11 +2,8 @@ import { AuthWS } from './auth_ws.js';
 import { Widget } from './widget.js';
 import { showModal } from './modal.js';
 
-console.log("Command Start");
-
 let modal = document.querySelector("#command-modal")
 
-console.log("Command fn");
 function addMessage(direction, msg) {
   let div = document.createElement("div")
   div.classList.add("message")
@@ -16,12 +13,10 @@ function addMessage(direction, msg) {
   modal.querySelector(".messages").prepend(div)
 }
 
-console.log("Command init");
 export let command = new Widget("command", function() {
   showModal("command-modal")
   modal.querySelector("input").focus()
 })
-console.log("Command set socket");
 command.socket = new AuthWS("JarvisChannel", {
   onmessage: function(msg) {
     if (msg.say) { addMessage("in", msg.say) }
@@ -34,18 +29,11 @@ command.socket = new AuthWS("JarvisChannel", {
   }
 })
 
-console.log("Start timeout");
-setTimeout(function() {
-  console.log("Finish timeout");
-  modal.querySelector("input").addEventListener("keypress", function(evt) {
-    console.log("Added listener");
-    if (evt.key == "Enter") {
-      let input = modal.querySelector("input")
-      command.socket.send({ action: "command", words: input.value })
-      addMessage("out", input.value)
-      input.value = ""
-    } else {
-      console.log(evt.key, evt.which);
-    }
-  })
-}, 300)
+modal.querySelector("input").addEventListener("keypress", function(evt) {
+  if (evt.key == "Enter") {
+    let input = modal.querySelector("input")
+    command.socket.send({ action: "command", words: input.value })
+    addMessage("out", input.value)
+    input.value = ""
+  }
+})
