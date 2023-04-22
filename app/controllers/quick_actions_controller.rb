6c15f9ad5d4d53470103b7dc7860c0e8dda8e312
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-04-21 22:40:44 -0600

    Redirect if not logge din

--- app/controllers/quick_actions_controller.rb
+++ app/controllers/quick_actions_controller.rb
@@ -2,4 +2,12 @@ class QuickActionsController < ::ActionController::Base
   include AuthHelper
   helper_method :current_user, :user_signed_in?
   layout "quick_actions"
+
+  before_action :redirect_to_login
+
+  private
+
+  def redirect_to_login
+    redirect_to login_path if current_user.blank?
+  end
 end
