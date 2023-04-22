7968828723f647f7ce68af381d340f1d40e1e1b1
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-04-21 18:16:03 -0600

    Lots of logs

--- public/quick_actions.js
+++ public/quick_actions.js
@@ -1,3 +1,6 @@
 import { garage } from './quick_actions/garage.js';
 import { tesla } from './quick_actions/tesla.js';
 import { command } from './quick_actions/command.js';
+
+console.log("Done imports");
+console.log("cmd", command);
--- public/quick_actions/command.js
+++ public/quick_actions/command.js
@@ -2,8 +2,11 @@ import { AuthWS } from './auth_ws.js';
 import { Widget } from './widget.js';
 import { showModal } from './modal.js';
 
+console.log("Command Start");
+
 let modal = document.querySelector("#command-modal")
 
+console.log("Command fn");
 function addMessage(direction, msg) {
   let div = document.createElement("div")
   div.classList.add("message")
@@ -13,15 +16,15 @@ function addMessage(direction, msg) {
   modal.querySelector(".messages").prepend(div)
 }
 
+console.log("Command init");
 export let command = new Widget("command", function() {
   showModal("command-modal")
   modal.querySelector("input").focus()
 })
+console.log("Command set socket");
 command.socket = new AuthWS("JarvisChannel", {
   onmessage: function(msg) {
-    if (msg.say) {
-      addMessage("in", msg.say)
-    }
+    if (msg.say) { addMessage("in", msg.say) }
   },
   onopen: function() {
     command.connected()
