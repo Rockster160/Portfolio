2d65c4a6dbf57654ef0d7e88bbec4922f47f4f05
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-04-21 23:15:33 -0600

    Fixed Twilio issues

--- app/workers/sms_worker.rb
+++ app/workers/sms_worker.rb
@@ -9,13 +9,13 @@ class SmsWorker
     api = Twilio::REST::Client.new(ENV['PORTFOLIO_TWILIO_ACCOUNT_SID'], ENV['PORTFOLIO_TWILIO_AUTH_TOKEN'])
 
     begin
-      api.account.messages.create({
+      api.messages.create(**{
         body: msg,
         to: to,
         media_url: Array.wrap(media).presence,
         from: "+18018500855"
       }.compact)
-    rescue Twilio::REST::RequestError => e
+    rescue Twilio::REST::RestError => e
       error_message = "Text Message failed!\nTo: #{to}\nMessage: #{msg}\n\nReason: #{e.message}"
       SmsWorker.perform_async("3852599640", error_message)
       Rails.logger.warn e
