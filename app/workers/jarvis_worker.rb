7bc496fe1dccbaa1034a673ff4173bc7a1012c5e
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-04-26 21:47:35 -0600

    More debugging

--- app/workers/jarvis_worker.rb
+++ app/workers/jarvis_worker.rb
@@ -2,13 +2,17 @@ class JarvisWorker
   include Sidekiq::Worker
 
   def perform(user_id, msg)
+    # user_id, msg = 1, {"event"=>{"name"=>"Laundry", "uid"=>"unix:1682567100:98D625D1-4BEB-4EAD-9852-C7353A333CA2", "type"=>"calendar", "notes"=>nil, "user_id"=>1, "scheduled_time"=>"2023-04-26T21:45:00.000-06:00"}}
     parsed = SafeJsonSerializer.load(msg)
-
     case parsed
-    when String then ::Jarvis.command(User.find(user_id), parsed)
+    when String
+      puts "\e[36m[LOGIT] | parsed is a string:[#{parsed.class}]#{parsed}\e[0m"
+      ::Jarvis.command(User.find(user_id), parsed)
     when Hash
+      puts "\e[36m[LOGIT] | parsed is a hash:[#{parsed.class}]#{parsed}\e[0m"
       event_data = parsed[:event]
       if event_data[:user_id].present? && event_data[:type].present?
+        puts "\e[36m[LOGIT] | calling trigger:[#{parsed.class}]#{parsed}\e[0m"
         ::Jarvis.trigger(
           event_data[:type],
           { input_vars: { "Event Data": event_data.except(:type, :user_id) } },
