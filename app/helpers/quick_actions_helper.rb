0986209ea8c20c468204c4ccefb345398ac7f3eb
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-07-16 18:02:27 -0600

    Extracted html safe

--- app/helpers/quick_actions_helper.rb
+++ app/helpers/quick_actions_helper.rb
@@ -8,10 +8,10 @@ module QuickActionsHelper
   end
 
   def emoji(icon)
-    "<i class=\"emoji\">#{icon}</i>".html_safe
+    "<i class=\"emoji\">#{icon}</i>"
   end
 
   def emoji_stack(*icons)
-    "<div class=\"emoji-stack\">#{icons.map { |i| emoji(i) }.join("")}</div>".html_safe
+    "<div class=\"emoji-stack\">#{icons.map { |i| emoji(i) }.join("")}</div>"
   end
 end
