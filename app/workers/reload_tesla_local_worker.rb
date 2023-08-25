cff8968efeb5f33c54aef83a035a1b5a641a62f7
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-08-25 13:52:36 -0600

    Added a notification message

--- app/workers/reload_tesla_local_worker.rb
+++ app/workers/reload_tesla_local_worker.rb
@@ -3,6 +3,7 @@ class ReloadTeslaLocalWorker
   sidekiq_options retry: false
 
   def perform
+    puts "[\e[33m#{Time.current.to_formatted_s(:short_with_time)}\e[0m] \e[36mTriggering Tesla Reload!\e[0m"
     TeslaControl.local
   end
 end
