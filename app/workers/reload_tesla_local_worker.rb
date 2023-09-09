16e70179f3b01f6a88432376fbf76b48bf1b6968
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-09-08 18:27:52 -0600

    Formatted time for Tesla worker

--- app/workers/reload_tesla_local_worker.rb
+++ app/workers/reload_tesla_local_worker.rb
@@ -3,7 +3,10 @@ class ReloadTeslaLocalWorker
   sidekiq_options retry: false
 
   def perform
-    puts "[\e[33m#{Time.current.to_formatted_s(:short_with_time)}\e[0m] \e[36mTriggering Tesla Reload!\e[0m"
+    Time.use_zone(User.timezone) {
+      now = Time.current.to_formatted_s(:short_with_time)
+      puts "[\e[33m#{now}\e[0m] \e[36mTriggering Tesla Reload!\e[0m"
+    }
     TeslaControl.local
   end
 end
