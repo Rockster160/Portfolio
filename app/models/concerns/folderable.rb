71ef1071945721969046da625c8cba111fceb634
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-10-10 14:13:07 -0600

    Extracted common methods for nested objects

---
+++ app/models/concerns/folderable.rb
@@ -0,0 +1,37 @@
+module Folderable
+  extend ActiveSupport::Concern
+
+  included do
+    orderable sort_order: :asc, scope: ->(obj) { obj.scoped_parent.public_send(obj.class.table_name) }
+
+    def breadcrumbs
+      parent = folder
+      [].tap { |trail|
+        loop do
+          break if parent.blank?
+          trail << parent
+          parent = parent.folder
+        end
+      }
+    end
+
+    def scoped_parent
+      folder || user
+    end
+
+    def tag_strings
+      tags.map(&:name).join(", ")
+    end
+
+    def tag_strings=(new_strings)
+      new_tag_names = new_strings.split(",").map { |str| str.strip.presence }.compact
+      tag_objects = new_tag_names.map do |tag_name|
+        Tag.find_or_create_by(name: tag_name.downcase)
+      end
+      self.tags = tag_objects
+      removed_tags = tags - tag_objects
+      removed_tags.each { |tag| tags.delete(tag) }
+      new_strings
+    end
+  end
+end
--- app/models/page.rb
+++ app/models/page.rb
@@ -14,43 +14,12 @@
 #
 class Page < ApplicationRecord
   include Orderable
+  include Folderable
 
   belongs_to :folder, optional: true
   belongs_to :user
   has_many :page_tags
   has_many :tags, through: :page_tags
 
-  orderable sort_order: :asc, scope: ->(page) { page.scoped_parent.pages }
-
   before_save -> { self.parameterized_name = name.parameterize }
-
-  def breadcrumbs
-    parent = folder
-    [].tap { |trail|
-      loop do
-        break if parent.blank?
-        trail << parent
-        parent = parent.folder
-      end
-    }
-  end
-
-  def scoped_parent
-    folder || user
-  end
-
-  def tag_strings
-    tags.map(&:name).join(", ")
-  end
-
-  def tag_strings=(new_strings)
-    new_tag_names = new_strings.split(",").map { |str| str.strip.presence }.compact
-    tag_objects = new_tag_names.map do |tag_name|
-      Tag.find_or_create_by(name: tag_name.downcase)
-    end
-    self.tags = tag_objects
-    removed_tags = tags - tag_objects
-    removed_tags.each { |tag| tags.delete(tag) }
-    new_strings
-  end
 end
