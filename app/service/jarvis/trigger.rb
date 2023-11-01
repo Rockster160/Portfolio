b9e5d07a0bcce4f57f8fa8cca30156a5ac8844b9
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-11-01 12:59:39 -0600

    Better run outputs

--- app/service/jarvis/trigger.rb
+++ app/service/jarvis/trigger.rb
@@ -1,6 +1,8 @@
 class Jarvis::Trigger < Jarvis::Action
   def attempt
     task = ::Jarvis::MatchTask.match_run(@user, @msg.gsub(/^\s*run:?\s+/i, ""), return_as: :task)
-    task&.then { |t| "Ran #{t.name}" }
+    task&.then { |t|
+      t.last_result.gsub(/^(true|false|Success|\d+)$/i, "").gsub(/\n+/, "\n").strip.presence || "Ran #{t.name}"
+    }
   end
 end
