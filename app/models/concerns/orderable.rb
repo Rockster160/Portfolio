72cb8b57a7a952167136738ee72647e1763f0ad3
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-11-14 08:38:26 -0700

    Quick revert

--- app/models/concerns/orderable.rb
+++ app/models/concerns/orderable.rb
@@ -2,26 +2,18 @@ module Orderable
   extend ActiveSupport::Concern
 
   included do
-    @orderable_scope = nil
-    @orderable_by_key = nil
-    @orderable_by_dir = :ASC
-
-    def self.orderable
-      {
-        scope: @orderable_scope,
-        key: @orderable_by_key,
-        dir: @orderable_by_dir,
-      }
-    end
+    @@orderable_scope = nil
+    @@orderable_by_key = nil
+    @@orderable_by_dir = :ASC
 
     def self.orderable_by(attr)
       key, dir = *attr.to_a.first if attr.is_a?(Hash)
-      @orderable_by_key = key || attr
-      @orderable_by_dir = ([dir&.upcase&.to_sym] & [:DESC, :ASC]).first || :ASC
+      @@orderable_by_key = key || attr
+      @@orderable_by_dir = ([dir&.upcase&.to_sym] & [:DESC, :ASC]).first || :ASC
     end
 
     def self.orderable_scope(method=nil, &block)
-      @orderable_scope = method || block
+      @@orderable_scope = method || block
     end
 
     # ordereable :sort_order
@@ -34,24 +26,24 @@ module Orderable
     end
 
     def orderable_ordered
-      if self.class.orderable[:scope].is_a?(Proc)
-        self.class.orderable[:scope].call(self)
-      elsif self.class.orderable[:scope].present?
-        send(self.class.orderable[:scope])
+      if @@orderable_scope.is_a?(Proc)
+        @@orderable_scope.call(self)
+      elsif @@orderable_scope.present?
+        send(@@orderable_scope)
       else
         self.class.all
       end
     end
 
     # Set default order column to `sort_order`
-    orderable_by(:sort_order) unless @orderable_by_key.present?
+    orderable_by(:sort_order) unless @@orderable_by_key.present?
     before_save :set_orderable # Callback to set the order column
     scope :ordered, -> { # Add a scope that can be used to return in order
-      order(self.class.orderable[:key] => self.class.orderable[:dir])
+      order(@@orderable_by_key => @@orderable_by_dir)
     }
 
     def set_orderable
-      self[self.class.orderable[:key]] ||= orderable_ordered.maximum(self.class.orderable[:key]).to_i + 1
+      self[@@orderable_by_key] ||= orderable_ordered.maximum(@@orderable_by_key).to_i + 1
     end
   end
 end
