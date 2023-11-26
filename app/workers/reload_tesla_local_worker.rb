0e57ccc28e5cd09e0928da7b0745564a4caf8774
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-11-25 19:26:55 -0700

    Changed time format

--- app/workers/reload_tesla_local_worker.rb
+++ app/workers/reload_tesla_local_worker.rb
@@ -4,7 +4,7 @@ class ReloadTeslaLocalWorker
 
   def perform
     Time.use_zone(User.timezone) {
-      now = Time.current.to_formatted_s(:short_with_time)
+      now = Time.current.strftime("%-m/%-d/%y %l:%M %p")
       puts "[\e[33m#{now}\e[0m] \e[36mTriggering Tesla Reload!\e[0m"
     }
     TeslaControl.local
