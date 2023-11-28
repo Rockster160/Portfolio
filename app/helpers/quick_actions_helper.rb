496d38246a0067815e633912467b0585342401dc
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-11-28 16:37:27 -0700

    Finished up images and such

--- app/helpers/quick_actions_helper.rb
+++ app/helpers/quick_actions_helper.rb
@@ -23,6 +23,7 @@ module QuickActionsHelper
     md.to_s
       .gsub(/([\p{So}\p{Sk}\p{Sm}\p{Sc}\p{S}\p{C}]+)/) { |f| "<i class=\"emoji\">#{Regexp.last_match(1)}</i>" }
       .gsub(/\[ico (.*?)(( \w+: .*?;)*)\]/) { |f| "<i class=\"emoji ti ti-#{Regexp.last_match(1)}\" style=\"#{Regexp.last_match(2)}\"></i>" }
+      .gsub(/\[img (.*?)\]/) { |f| emoji("<img src=\"./#{Regexp.last_match(1)}.png\"/>") }
       .html_safe
   end
 
