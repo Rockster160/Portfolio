c5ce3958d6008232266ee1cd6126b57bdcdb7fd8
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-12-04 23:27:04 -0700

    Use params not data

--- app/channels/socket_channel.rb
+++ app/channels/socket_channel.rb
@@ -9,14 +9,14 @@ class SocketChannel < ApplicationCable::Channel
 
   def receive(data)
     data.deep_symbolize_keys!
-    return unless data[:channel_id].present?
+    return unless params[:channel_id].present?
 
     ::Jarvis.execute_trigger(
       :websocket,
       { input_vars: { "WS Receive Data" => data.reverse_merge(params) } },
       scope: [
         "user_id = #{current_user.id} AND (input ~* ? OR input = '*')",
-        "\\m#{data[:channel_id]}\\M"
+        "\\m#{params[:channel_id]}\\M"
       ]
     )
   end
