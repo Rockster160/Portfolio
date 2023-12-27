85931042667da70d4a24dc454e12510758697550
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-12-27 00:18:27 -0700

    Rolled back safe json

--- app/models/concerns/safe_json_serializer.rb
+++ app/models/concerns/safe_json_serializer.rb
@@ -5,13 +5,17 @@ class SafeJsonSerializer
 
   def self.load(str)
     safe_str = str.then { |s| next s if s.nil? || s.is_a?(String); s.to_json }
+    safe_str.present? ? JSON.parse(safe_str, object_class: BetterJson) : str
 
-    return str unless safe_str.present?
-
-    json = JSON.parse(safe_str, object_class: BetterJson)
-    return json if json.is_a?(Hash) || json.is_a?(Array)
-
-    SafeJsonSerializer.load(json)
+    # start_str = str.dup
+    # safe_str = str.then { |s| next s if s.nil? || s.is_a?(String); s.to_json }
+    #
+    # return str unless safe_str.present?
+    #
+    # json = JSON.parse(safe_str, object_class: BetterJson)
+    # return json if json.is_a?(Hash) || json.is_a?(Array)
+    #
+    # start_str == json ? SafeJsonSerializer.load(json) : json
   rescue JSON::ParserError
     str
   end
