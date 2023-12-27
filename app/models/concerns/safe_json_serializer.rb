db82b6baebe098fcb0b7a7d3e2bfc22bc811d022
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-12-27 12:13:15 -0700

    Restored

--- app/models/concerns/safe_json_serializer.rb
+++ app/models/concerns/safe_json_serializer.rb
@@ -4,12 +4,8 @@ class SafeJsonSerializer
   end
 
   def self.load(str)
-    start_str = str.dup
     safe_str = str.then { |s| next s if s.nil? || s.is_a?(String); s.to_json }
-    json = safe_str.present? ? JSON.parse(safe_str, object_class: BetterJson) : str
-
-    return json if json.is_a?(BetterJson)
-    start_str == json ? json : SafeJsonSerializer.load(json)
+    safe_str.present? ? JSON.parse(safe_str, object_class: BetterJson) : str
   rescue JSON::ParserError
     str
   end
