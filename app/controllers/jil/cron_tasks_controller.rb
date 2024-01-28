181c8cc45a2b31fa9922ece698c19d75841d1fb1
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-01-28 16:39:37 -0700

    Order by next occurrence

--- app/controllers/jil/cron_tasks_controller.rb
+++ app/controllers/jil/cron_tasks_controller.rb
@@ -8,7 +8,7 @@ class Jil::CronTasksController < ApplicationController
   end
 
   def index
-    @tasks = current_user.cron_tasks.order(last_trigger_at: :desc)
+    @tasks = current_user.cron_tasks.order(next_trigger_at: :desc)
   end
 
   def new
