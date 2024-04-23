9f8c4cc56b8b074527b6d9401f686b36b869e761
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-04-23 12:12:57 -0600

    Better error backtrace

--- app/models/slack_notifier.rb
+++ app/models/slack_notifier.rb
@@ -1,14 +1,21 @@
-class SlackNotifier
-  def self.notify(message, channel: '#portfolio', username: 'Portfolio-Bot', icon_emoji: ':blackmage:', attachments: [])
+module SlackNotifier
+  module_function
+
+  def notify(message, channel: '#portfolio', username: 'Portfolio-Bot', icon_emoji: ':blackmage:', attachments: [])
     return puts("\e[31mSlack: #{message}\e[0m") if Rails.env.test?
 
     SlackWorker.perform_async(message, channel, username, icon_emoji, attachments)
   end
 
-  def self.err(exception, message="Error: ", channel: '#portfolio', username: 'Portfolio-Bot', icon_emoji: ':blackmage:', attachments: [])
+  def err(exception, message="Error: ", channel: '#portfolio', username: 'Portfolio-Bot', icon_emoji: ':blackmage:', attachments: [])
     SlackNotifier.notify(
       "#{message}\n*#{exception}*\n#{exception.message}\n" \
-      "```#{exception.backtrace.select { |l| l.include?("/app/") }.join("\n")}```"
+      "```#{format_exception(exception)}```"
     )
+  end
+
+  def format_exception(exception)
+    focused = exception.backtrace.select { |l| l.include?("/app/") }
+    focused.map { |line| line[/releases\/\d+(.*)/, 1] || line }.join("\n")
   end
 end
