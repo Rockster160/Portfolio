4986529efc530acad01f7feaf9a9dd9a902b76cd
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-05-04 19:42:39 -0600

    More repushes for notification updates

--- app/controllers/jil_prompts_controller.rb
+++ app/controllers/jil_prompts_controller.rb
@@ -1,6 +1,7 @@
 class JilPromptsController < ApplicationController
   before_action :authorize_user_or_guest
   before_action :set_prompt, except: :index
+  after_action :repush_notifications
 
   def index
     @prompts = current_user.prompts.unanswered.order(created_at: :desc)
@@ -11,18 +12,22 @@ class JilPromptsController < ApplicationController
     data = params.dig(:prompt, :response)&.permit!&.to_h || {}
     @prompt.update(response: data)
     @prompt.task&.execute(response: @prompt.response, params: @prompt.params)
-    ::WebPushNotifications.update_count(current_user)
 
     redirect_to jarvis_path
   end
 
   def destroy
     @prompt.destroy
-    ::WebPushNotifications.update_count(current_user)
     redirect_to jil_prompts_path
   end
 
   private
+
+  def repush_notifications
+    return unless user_signed_in?
+
+    ::WebPushNotifications.update_count(current_user)
+  end
 
   def set_prompt
     @prompt = current_user.prompts.find_by(id: params[:id])
