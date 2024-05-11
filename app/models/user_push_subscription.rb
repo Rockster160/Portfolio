2d04dd95a05b6bcb0da1a8181d1ad482fe26a4a8
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-05-10 23:58:09 -0600

    Another deploy test

--- app/models/user_push_subscription.rb
+++ app/models/user_push_subscription.rb
@@ -7,7 +7,7 @@
 #  endpoint      :string
 #  p256dh        :string
 #  registered_at :datetime
-#  sub_auth      :string # -- Delete this?
+#  sub_auth      :string
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #  user_id       :integer
