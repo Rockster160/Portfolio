b33b204f5fa8bac18377b37569bfc2f188791864
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-05-13 00:02:25 -0600

    Remove modal resize options

--- public/quick_actions/modal.js
+++ public/quick_actions/modal.js
@@ -9,17 +9,6 @@ export function hideCurrentModal() {
   open_modals[open_modals.length -1]?.classList?.remove("show")
 }
 
-export function resizeModal() {
-  if (!window.visualViewport) { return }
-
-  let modal = document.querySelector(".modal.show#command-modal")
-  if (modal) { modal.style.height = window.visualViewport.height - 40 + "px" }
-  setTimeout(function() {
-    let modal = document.querySelector(".modal.show#command-modal")
-    if (modal) { modal.style.height = window.visualViewport.height - 40 + "px" }
-  }, 600)
-}
-
 document.addEventListener("click", function(evt) {
   if (evt.target.closest(".close")) {
     evt.target.closest(".modal").classList.remove("show")
@@ -27,19 +16,6 @@ document.addEventListener("click", function(evt) {
   if (evt.target.tagName == "BODY") {
     hideCurrentModal()
   }
-})
-
-window.addEventListener("resize", resizeModal)
-window.addEventListener("focusout", function() {
-  // document.querySelector(".modal.show")?.classList?.remove("show")
-  resizeModal()
-})
-document.querySelectorAll("input").forEach((input) => {
-  input.addEventListener("focus", resizeModal)
-  input.addEventListener("blur", resizeModal)
-})
-document.querySelectorAll(".modal").forEach((modal) => {
-  modal.addEventListener("transitionend", resizeModal)
 })
 
 document.addEventListener("keydown", function(event) {
