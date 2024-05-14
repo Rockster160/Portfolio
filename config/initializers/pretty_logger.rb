c30e1758e9d52ca9cdcfaaafab3ec964637d599d
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-05-13 18:11:05 -0600

    More colors

--- config/initializers/pretty_logger.rb
+++ config/initializers/pretty_logger.rb
@@ -2,6 +2,7 @@
 ::PrettyLogger::RequestLogger.class_eval do
   def pretty_user
     return colorize(:grey, "[?]") unless current_user.present?
+    return colorize(:grey, "Guest:#{current_user.id}") if current_user.guest?
 
     name = current_user.try(:username).presence
     name ||= "#{current_user.guest? ? :Guest : :User}:#{current_user.id}"
@@ -10,7 +11,9 @@
     when 1 then colorize(:rocco, "[Me]")
     when 4 then colorize(:purple, "[Mom]")
     when 33529 then colorize(:magenta, "[Janaya]")
-    else colorize(:olive, "#{name}")
+    when 34226 then colorize(:pink, "[S]") # Saya
+    when 37764 then colorize(:yellow, "[C]") # Carlos
+    else colorize(:olive, "[#{name}]")
     end
   end
 end
