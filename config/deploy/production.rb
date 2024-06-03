a582b1d1202694cde040f29d674d08f8bf055b6d
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-06-02 18:33:27 -0600

    Updated ip for Github

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # New person: Add their `id_rsa.pub` (or other desired key) to
 # `Portfolio:/home/deploy/.ssh/authorized_keys`
-server "138.68.3.152", user: :deploy, roles: [:app, :web, :db]
+server "165.227.15.235", user: :deploy, roles: [:app, :web, :db]
 
 set :stage,           :production
 set :rails_env,       :production
