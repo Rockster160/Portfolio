cbd0f692bd3deba1837dba9dd9f57e46fab833f5
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-06-05 18:40:22 -0600

    Log WS

--- app/channels/socket_channel.rb
+++ app/channels/socket_channel.rb
@@ -32,6 +32,14 @@ class SocketChannel < ApplicationCable::Channel
   end
 
   def receive(data)
+    identifier = (
+      if current_user
+        "\e[36m[#{current_user.try(:username).presence || "User #{current_user.try(:id)}"}]"
+      else
+        "\e[31m[?]"
+      end
+    )
+    ::PrettyLogger.info("\e[35m[WS]#{identifier}\n#{PrettyLogger.pretty_message(data.deep_symbolize_keys)}")
     trigger(data, :receive)
   end
 
--- app/service/memoizeable.rb
+++ app/service/memoizeable.rb
@@ -3,7 +3,7 @@ module Memoizeable
     base.class_eval do
       def self.memoize(memos)
         memos.each do |memo, block|
-          memo_ivar = :"@#{memo}"
+          memo_ivar = :"@_#{memo}"
           define_method(memo) do
             return instance_variable_get(memo_ivar) if instance_variable_defined?(memo_ivar)
 
