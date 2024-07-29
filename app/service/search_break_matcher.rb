2fed7797263ef18e11e97fa0f84bd2963ab6f3f3
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-07-29 10:03:12 -0600

    Fixed breaker to allow for not matchers

--- app/service/search_break_matcher.rb
+++ app/service/search_break_matcher.rb
@@ -25,12 +25,13 @@ class SearchBreakMatcher
 
     match?
   rescue StandardError => e
+    # binding.pry
     raise unless Rails.env.production?
   end
 
   def match?
     return @matched if defined?(@matched)
-    return @matched = false if !@top_data.is_a?(Hash) || @top_data.keys.none?
+    return @matched = false if !@top_data.is_a?(::Hash) || @top_data.keys.none?
     raise "Only 1 top level key allowed" unless @top_data.keys.one?
 
     @matched = breaker_matches(@top_breaker, @top_data).any?
@@ -46,62 +47,70 @@ class SearchBreakMatcher
   # {piece} -- [String, {breaker}]
 
   def breaker_matches(breaker, data, passed_delim=nil)
-    if (breaker.is_a?(String) || breaker.is_a?(Symbol)) && passed_delim.present?
-      return valstr_match(breaker, passed_delim, data)
+    if (breaker.is_a?(::String) || breaker.is_a?(::Symbol)) && passed_delim.present?
+      return valstr_matches(breaker, passed_delim, data)
     end
 
     (breaker[:keys]&.filter_map { |val, broken| # val="event" && broken[:contains] = [{piece}]
       if passed_delim.present?
-        data = valstr_match(val, passed_delim, data)
+        data = valstr_matches(val, passed_delim, data)
         next false if !data
       end
 
-      top_match = broken.empty? && valstr_match(val, :contains, data)
+      top_match = broken.empty? && valstr_matches(val, :contains, data)
       next top_match if top_match
 
       broken.filter_map { |delim, piece|
         case piece
-        when Array then piece.filter_map { |nested_breaker| breaker_matches(nested_breaker, data, delim) }
-        else valstr_match(val, delim, data)
+        when ::Array then piece.filter_map { |nested_breaker| breaker_matches(nested_breaker, data, delim) }
+        else valstr_matches(val, delim, data)
         end
       }
     }&.flatten || []) + (breaker[:vals]&.filter_map { |delim, piece|
       case piece
-      when Array then piece.filter_map { |nested_breaker| breaker_matches(nested_breaker, data, delim) }
-      when String, Symbol then valstr_match(piece, delim, data)
+      when ::Array then piece.filter_map { |nested_breaker| breaker_matches(nested_breaker, data, delim) }
+      when ::String, ::Symbol then valstr_matches(piece, delim, data)
       else
         raise "Unhandled piece class: #{piece.class}"
       end
     }&.flatten || [])
   rescue StandardError => e
+    # binding.pry
     raise unless Rails.env.production?
   end
 
-  def valstr_match(val, delim, data) # val is a bottom-level string from the {broken} data
-    data = data.to_s if data.is_a?(Symbol)
-    return check_match?(val, delim, data) && data if data.is_a?(String)
-    data.is_a?(Hash) && data.find { |dkey, dvals|
-      break dvals if check_match?(val, delim, dkey, dvals)
+  def valstr_matches(val, delim, data) # val is a bottom-level string from the {broken} data
+    if delim.to_s.starts_with?("not_")
+      invert_delim = delim.to_s[4..].to_sym
+      # Do something for `not` by itself?
+      return if valstr_matches(val, invert_delim, data).present?
+    end
+    return data.filter_map { |d| valstr_matches(val, delim, d) }.flatten if data.is_a?(::Array)
+    data = data.to_s if data.is_a?(::Symbol)
+    return check_match?(val, delim, data) && data if data.is_a?(::String)
+    data.is_a?(::Hash) && data.filter_map { |dkey, dvals|
+      next dvals if check_match?(val, delim, dkey, dvals)
 
       nested_val = (
         case dvals
-        when Hash
-          dvals.find { |k,v|
-            break v if check_match?(val, delim, k, v)
-            matched_val = valstr_match(val, delim, v)
-            break v if matched_val
+        when ::Hash
+          dvals.filter_map { |k,v|
+            next {k => v} if check_match?(val, delim, k, v)
+            matched_val = valstr_matches(val, delim, v)
+            next v if matched_val
           }
-        when Array
-          dvals.find { |dv| break dv if valstr_match(val, delim, dv) }
-        when String # string to string -- Check if they match according to the delim
+        when ::Array
+          dvals.filter_map { |dv| next dv if valstr_matches(val, delim, dv) }
+        when ::String # string to string -- Check if they match according to the delim
           check_match?(val, delim, dvals) && dvals
         else
           raise "Unhandled dvals class: #{dvals.class}"
         end
       )
-      break nested_val if nested_val
-    } || false
+      next nested_val if nested_val
+    }.presence
   rescue StandardError => e
+    # binding.pry
     raise unless Rails.env.production?
   end
 
@@ -130,8 +139,11 @@ class SearchBreakMatcher
         SearchBreakMatcher.new(single_val, { str => {} }, @parent || self).match?
       }
     else false
-    end
+    end.tap { |b|
+      puts "\e[3#{b ? 2 : 1}m#{val}[#{delim}]#{str}\e[0m"
+    }
   rescue StandardError => e
+    # binding.pry
     raise unless Rails.env.production?
   end
 end
