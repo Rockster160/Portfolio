5158e8108bb5394e5857ae5db08f7297e6cf9170
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-08-21 19:50:25 -0600

    Reset Tasks

---
+++ db/migrate/20240822014938_reset_jil_task.rb
@@ -0,0 +1,34 @@
+class ResetJilTask < ActiveRecord::Migration[7.1]
+  def change
+    drop_table :jil_tasks
+    drop_table :jil_executions
+
+    create_table :jil_tasks do |t|
+      t.uuid :uuid, default: -> { "uuid_generate_v4()" }
+      t.belongs_to :user
+      t.integer :sort_order
+      t.text :name
+      t.text :cron
+      t.text :listener
+      t.text :code
+      t.boolean :enabled, default: true
+      t.datetime :next_trigger_at
+      t.datetime :last_trigger_at
+
+      t.timestamps
+    end
+
+    create_table :jil_executions do |t|
+      t.belongs_to :user
+      t.belongs_to :jil_task
+      t.integer :status, default: 0
+      t.jsonb :input_data
+      t.text :code
+      t.jsonb :ctx
+      t.datetime :started_at, default: -> { "CURRENT_TIMESTAMP" }
+      t.datetime :finished_at
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_16_055003) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_22_014938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
