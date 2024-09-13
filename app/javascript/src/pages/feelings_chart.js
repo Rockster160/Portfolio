a516bf08e51e31fe82ea9d16b56d271d70c9c78a
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-09-13 00:22:30 -0600

    Max 100

--- app/javascript/src/pages/feelings_chart.js
+++ app/javascript/src/pages/feelings_chart.js
@@ -42,6 +42,8 @@ $(document).ready(function() {
             }
           },
           y: {
+            max: 100,
+            min: 0,
             beginAtZero: true,
             title: {
               display: true,
@@ -51,42 +53,4 @@ $(document).ready(function() {
         }
       }
     });
-  // const config = {
-  //   type: "bar",
-  //   data: feelings_data,
-  //   options: {
-  //     scales: {
-  //       y: {
-  //         max: Math.max(100, ...feelings_data.datasets[0].data),
-  //         min: 0,
-  //         grid: { color: "#0140AA" },
-  //       },
-  //     },
-  //     plugins: {
-  //       legend: { display: false },
-  //       horzLine: {
-  //         y: 100,
-  //         color: "#0140AA",
-  //         width: 5,
-  //       }
-  //     },
-  //   },
-  //   plugins: [{
-  //     id: "horzLine",
-  //     beforeDraw(chart, args, options) {
-  //       const {ctx, chartArea: {left, top, width, height}} = chart
-  //       let yPixel = chart.scales.y.getPixelForValue(options.y)
-  //       ctx.save()
-  //       ctx.beginPath()
-  //       ctx.strokeStyle = options.color
-  //       ctx.lineWidth = options.width
-  //       ctx.moveTo(left - 5, yPixel)
-  //       ctx.lineTo(left + width - 5, yPixel)
-  //       ctx.stroke()
-  //       ctx.restore()
-  //     }
-  //   }]
-  // }
-  //
-  // let chart = new Chart(ctx, config)
 })
