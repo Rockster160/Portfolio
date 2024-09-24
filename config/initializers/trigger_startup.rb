8ecedba0add90b6bbc30d0874a1089853249e86e
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-09-24 12:17:00 -0600

    Fixed startup script

--- config/initializers/trigger_startup.rb
+++ config/initializers/trigger_startup.rb
@@ -1,7 +1,11 @@
-::Jil::Executor.async_trigger(User.me, :startup, {
-  merge: `git rev-parse HEAD`.strip,
-  **(`git log --no-merges -n 1 --format="%H|%an|%s"`.strip.then { |raw|
-    hash, author, message = raw.split("|")
-    { hash: hash, author: author, message: message }
-  })
-})
+Rails.application.config.after_initialize do
+  if defined?(Rails::Server)
+    ::Jil::Executor.async_trigger(User.me, :startup, {
+      merge: `git rev-parse HEAD`.strip,
+      **(`git log --no-merges -n 1 --format="%H|%an|%s"`.strip.then { |raw|
+        hash, author, message = raw.split("|")
+        { hash: hash, author: author, message: message }
+      })
+    })
+  end
+end
