2379391e01eca79db1bedae50eab68f42c71cf97
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-10-18 09:52:53 -0600

    Attempt to only run startup command once

--- config/initializers/trigger_startup.rb
+++ config/initializers/trigger_startup.rb
@@ -1,5 +1,5 @@
 Rails.application.config.after_initialize do
-  if defined?(Puma) && !Rails.env.test? && !Rails.const_defined?("Console")
+  if defined?(Puma) && !Puma.respond_to?(:cli_config) && !Rails.env.test? && !Rails.const_defined?("Console")
     ::Jil.trigger(User.me, :startup, {
       merge: `git rev-parse HEAD`.strip,
       **(`git log --no-merges -n 1 --format="%H|%an|%s"`.strip.then { |raw|
