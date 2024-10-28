be619967d2099297be218d93a98c8daad70b7fe3
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-10-28 11:37:05 -0600

    Updated error notiier

--- lib/custom_notifier.rb
+++ lib/custom_notifier.rb
@@ -1,54 +1,70 @@
 module ExceptionNotifier
   class CustomNotifier
-
     def initialize(options)
     end
 
     def call(exception, options={})
-      exception_name = "#{exception.class.to_s =~ /^[aeiou]/i ? 'An' : 'A'} `#{exception.class.to_s}`"
+      exception_name = "#{exception.class.to_s =~ /^[aeiou]/i ? "An" : "A"} `#{exception.class.to_s}`"
 
       if options[:env].nil?
         text = "#{exception_name} occurred in background\n"
       else
         env = options[:env]
-        data = (env['exception_notifier.exception_data'] || {}).merge(options[:data] || {})
-        kontroller = env['action_controller.instance']
-        request = env['action_dispatch.request']
+        data = (env["exception_notifier.exception_data"] || {}).merge(options[:data] || {})
+        kontroller = env["action_controller.instance"]
+        request = env["action_dispatch.request"]
         if data[:current_user]
-          text = "*#{data[:current_user].try(:username)}* experienced #{exception_name} while `#{env['REQUEST_METHOD']} <#{env['REQUEST_URI']}>`"
+          text = "*#{data[:current_user].try(:username)}* experienced #{exception_name}"
         else
-          text = "#{exception_name} occurred while `#{env['REQUEST_METHOD']} <#{env['REQUEST_URI']}>`"
+          text = "[#{request&.ip || "?.?.?.?"}] #{exception_name} occurred"
         end
-        text += " was processed by `#{kontroller.controller_name}##{kontroller.action_name}`" if kontroller
-        text += "\n"
+        text += " from `#{env["REQUEST_METHOD"]} <#{env["REQUEST_URI"]}>`"
+        if kontroller
+          text += " was processed by `#{kontroller.controller_name}##{kontroller.action_name}`"
+        end
         params = data[:params] || request&.params
         if params
           params = params.permit!.to_h if params.is_a?(::ActionController::Parameters)
           begin
-            text += "```\n#{JSON.pretty_generate(params)}\n```"
+            text += "\n```\n#{JSON.pretty_generate(params)}\n```\n"
           rescue StandardError
             str = "#{params}".truncate(2000)
-            text += "```\n#{str}\n```"
+            text += "\n```\n#{str}\n```\n"
           end
-          text += "\n"
         end
       end
 
       clean_message = exception.message.gsub("`", "'")
-      fields = [{ title: 'Exception', value: clean_message }]
+      fields = [{ title: "Exception", value: clean_message }]
 
-      if exception.backtrace
-        fields.push({ title: 'Focused Backtrace', value: exception.backtrace.map { |l|
-          l.include?("/app/") ? l.gsub("`", "'").gsub(/^.*\/app\//, "") : nil
-        }.compact.join("\n") })
+      focused_backtrace = focused_trace(exception.backtrace).presence
+      if focused_backtrace
+        fields.push({ title: "Focused Backtrace", value: focused_backtrace.join("\n") })
+      else
+        fields.push({ title: "Focused Caller", value: focused_trace(caller).join("\n") })
       end
 
       exception_message = fields.map { |h| "*#{h[:title]}*\n#{h[:value]}" }.join("\n\n")
-      attchs = [color: 'danger', text: exception_message, mrkdwn_in: %w(text fields)]
+      attchs = [color: "danger", text: exception_message, mrkdwn_in: %w(text fields)]
 
-      environ = Rails.env.production? ? '' : " [#{Rails.env.upcase}]"
-      ::SlackNotifier.notify(text, channel: '#portfolio', username: "Portfolio-Bot#{environ}", icon_emoji: ':blackmage::', attachments: attchs)
+      environ = Rails.env.production? ? "*[PROD]*" : "_[#{Rails.env.upcase}]_"
+      ::SlackNotifier.notify(text, channel: "#portfolio", username: "Portfolio-Bot#{environ}", icon_emoji: ":blackmage::", attachments: attchs)
     end
 
+    def focused_trace(trace, before: 10, after: 5)
+      return [] if !trace
+
+      trace.select { |line|
+        line.to_s.include?(Rails.root.to_s)
+      }.map { |line|
+        line.gsub(/^.*?#{Rails.root}/, "").gsub(/(app)?\/app\//, "app/").gsub(":in `", " `").gsub(/(:\d+) .*?$/, '\1')
+      }.then { |list|
+        if list.length > (before + after + 1)
+          list[..before] + ["..."] + list[-after..]
+        else
+          list
+        end
+      }
+    end
   end
 end
