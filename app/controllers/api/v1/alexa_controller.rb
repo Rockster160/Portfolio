6b825c57622cc92449d73f02de3fd1e800745050
Rocco Nicholls <rocco11nicholls@gmail.com>
2024-12-20 18:08:43 -0700

    Attempt to change alexa endpoint

--- app/controllers/api/v1/alexa_controller.rb
+++ app/controllers/api/v1/alexa_controller.rb
@@ -1,5 +1,47 @@
 class Api::V1::AlexaController < Api::V1::BaseController
   def alexa
-    head :ok
+    render json: alexa_response("Success")
+  end
+
+  private
+
+  def alexa_response(words)
+    words = words.to_s.presence || "No response from Jarvis"
+    {
+      version: "1.0",
+      # sessionAttributes: {
+      #   key: "value"
+      # },
+      response: {
+        outputSpeech: {
+          type: "PlainText",
+          text: words.split("\n").first(2).join("\n"), # Only return the first item
+          # playBehavior: "REPLACE_ENQUEUED"
+        },
+        # card: {
+        #   type: "Standard",
+        #   title: "Title of the card",
+        #   text: "Text content for a standard card",
+        #   image: {
+        #     smallImageUrl: "https://url-to-small-card-image...",
+        #     largeImageUrl: "https://url-to-large-card-image..."
+        #   }
+        # },
+        # reprompt: {
+        #   outputSpeech: {
+        #     type: "PlainText",
+        #     text: "Plain text string to speak",
+        #     playBehavior: "REPLACE_ENQUEUED"
+        #   }
+        # },
+        # directives: [
+        #   {
+        #     type: "InterfaceName.Directive"
+        #     (...properties depend on the directive type)
+        #   }
+        # ],
+        shouldEndSession: true
+      }
+    }
   end
 end
