2f4f8c4696141dfdd177dbc7050491eecd5601e8
Rocco Nicholls <rocco11nicholls@gmail.com>
2025-01-16 16:11:25 -0700

    Updated cap stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.18.0"
+lock "~> 3.19.2"
 
 set :application, "portfolio"
 set :repo_url, "https://github.com/Rockster160/portfolio.git"
