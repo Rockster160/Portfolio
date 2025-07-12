483b6aff71b9fd4fbe45e2c37a97592fe3cdb64e
Rocco Nicholls <rocco11nicholls@gmail.com>
2025-07-11 20:26:00 -0600

    Better lists

--- app/javascript/src/pages/dashboard/cells/todo.js
+++ app/javascript/src/pages/dashboard/cells/todo.js
@@ -3,3 +3,11 @@ import { ListCell } from "./_list"
 (function() {
   new ListCell("Todo")
 })()
+
+(function() {
+  new ListCell("To Do Today")
+})()
+
+(function() {
+  new ListCell("Happy House")
+})()
