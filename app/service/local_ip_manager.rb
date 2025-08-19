8e023e225042d15bd3e10c6c074af69a644a713c
Rocco Nicholls <rocco11nicholls@gmail.com>
2025-08-18 21:50:00 -0600

    Quick IP updates

--- app/service/local_ip_manager.rb
+++ app/service/local_ip_manager.rb
@@ -1,23 +1,28 @@
 module LocalIpManager
   module_function
 
+  def local_ip
+    DataStorage[:local_ip]
+  end
+
   def local_ip=(new_ip)
     old_ip = DataStorage[:local_ip]
     return if new_ip.to_s == "::1"
     return if old_ip.to_s == new_ip.to_s
 
+    if Rails.env.production?
+        # ardesian.duckdns.org
+      HTTParty.get(
+        "https://www.duckdns.org/update?" + {
+          domains: "ardesian",
+          token: DataStorage[:duckdns_token],
+          ip: new_ip
+        }.to_query
+      )
+    end
+
     DataStorage[:local_ip] = new_ip
 
-    return unless Rails.env.production?
-
-    # ardesian.duckdns.org
-    HTTParty.get(
-      "https://www.duckdns.org/update?" + {
-        domains: "ardesian",
-        token: DataStorage[:duckdns_token],
-        ip: new_ip
-      }.to_query
-    )
     Jarvis.say("Updated IP Addresses! #{new_ip}")
   end
 end
