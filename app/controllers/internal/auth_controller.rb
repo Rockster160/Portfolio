a91c6d23d1430cedd4cf3dca214680e8599fd706
Rocco Nicholls <rocco11nicholls@gmail.com>
2025-08-22 01:34:28 -0600

    Updated to not redirect

--- app/controllers/internal/auth_controller.rb
+++ app/controllers/internal/auth_controller.rb
@@ -1,6 +1,5 @@
 class Internal::AuthController < ApplicationController
   skip_forgery_protection
-  before_action :authorize_admin
 
   def check
     head(current_user&.admin? ? :ok : :unauthorized)
