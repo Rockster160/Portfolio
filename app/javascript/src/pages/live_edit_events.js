a776c6d038f04c296cb40e1e1511c6bfe608b4bd
Rocco Nicholls <rocco11nicholls@gmail.com>
2025-08-25 10:13:17 -0600

    Only push updates on change

--- app/javascript/src/pages/live_edit_events.js
+++ app/javascript/src/pages/live_edit_events.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
   if ($(".live-edit-events").length == 0) { return }
 
-  $(".live-edit-events input, .live-edit-events textarea").blur(function() {
+  $(".live-edit-events input, .live-edit-events textarea").each(function() {
+    // Store the initial value
+    $(this).data("original-value", $(this).val())
+  }).blur(function() {
+    let originalValue = $(this).data("original-value")
+    let currentValue = $(this).val()
+    if (originalValue === currentValue) { return } // Do not update if unchanged
+
     let params = {}
-    params[$(this).attr("name")] = $(this).val()
+    params[$(this).attr("name")] = currentValue
 
     $.ajax({
       url: $(this).attr("data-update-url"),
       type: "PATCH",
       data: params
     })
+
+    // Update the stored value
+    $(this).data("original-value", currentValue)
   }).keydown(function(evt) {
     if ($(this).is("input") && evt.key == "Enter") {
       $(this).blur()
