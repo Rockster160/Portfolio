c40080e3935d22837b79fb581ca88f1fbe35965f
Rocco Nicholls <rocco11nicholls@gmail.com>
2025-12-19 10:21:26 -0700

    Attempt to rollback potential memory issues

--- app/channels/socket_channel.rb
+++ app/channels/socket_channel.rb
@@ -4,37 +4,51 @@ class SocketChannel < ApplicationCable::Channel
   CONNECTION_TIMEOUT = 10.seconds
   CONNECTION_PING_INTERVAL = 3.seconds
   periodically every: CONNECTION_PING_INTERVAL do
-    last_pong = connection.instance_variable_get(:@_last_pong_at)
-    timeout   = connection.instance_variable_get(:@_ws_timeout) || CONNECTION_TIMEOUT
-    driver = connection.instance_variable_get(:@_ws_driver)
-    driver&.ping
-    if last_pong && (Time.current - last_pong) > timeout
+    @driver&.ping
+    if Time.zone.now - @_last_request_at > @_timeout
       # close_connection
       # self.disconnect
-      trigger(params, :disconnected)
       connection.close
     end
   end
   def connection_watcher
-    # Ensure this only gets set up once per connection, not once per subscription
-    return if connection.instance_variable_get(:@_pong_watcher_setup)
-
-    connection.instance_variable_set(:@_pong_watcher_setup, true)
-    connection.instance_variable_set(:@_ws_timeout, CONNECTION_TIMEOUT)
-    connection.instance_variable_set(:@_last_pong_at, Time.current)
-
-    websocket = connection.instance_variable_get("@websocket")
-    driver = websocket&.possible?&.instance_variable_get("@driver")
-    connection.instance_variable_set(:@_ws_driver, driver)
-
     @_last_request_at ||= Time.zone.now
     @_timeout = CONNECTION_TIMEOUT
     @driver = connection.instance_variable_get("@websocket").possible?&.instance_variable_get("@driver")
-
-    driver&.on(:pong) do
-      connection.instance_variable_set(:@_last_pong_at, Time.current)
-    end
+    @driver.on(:pong) { @_last_request_at = Time.zone.now }
   end
+  # periodically every: CONNECTION_PING_INTERVAL do
+  #   last_pong = connection.instance_variable_get(:@_last_pong_at)
+  #   timeout   = connection.instance_variable_get(:@_ws_timeout) || CONNECTION_TIMEOUT
+  #   driver = connection.instance_variable_get(:@_ws_driver)
+  #   driver&.ping
+  #   if last_pong && (Time.current - last_pong) > timeout
+  #     # close_connection
+  #     # self.disconnect
+  #     trigger(params, :disconnected)
+  #     connection.close
+  #   end
+  # end
+  # def connection_watcher
+  #   # Ensure this only gets set up once per connection, not once per subscription
+  #   return if connection.instance_variable_get(:@_pong_watcher_setup)
+
+  #   connection.instance_variable_set(:@_pong_watcher_setup, true)
+  #   connection.instance_variable_set(:@_ws_timeout, CONNECTION_TIMEOUT)
+  #   connection.instance_variable_set(:@_last_pong_at, Time.current)
+
+  #   websocket = connection.instance_variable_get("@websocket")
+  #   driver = websocket&.possible?&.instance_variable_get("@driver")
+  #   connection.instance_variable_set(:@_ws_driver, driver)
+
+  #   @_last_request_at ||= Time.zone.now
+  #   @_timeout = CONNECTION_TIMEOUT
+  #   @driver = connection.instance_variable_get("@websocket").possible?&.instance_variable_get("@driver")
+
+  #   driver&.on(:pong) do
+  #     connection.instance_variable_set(:@_last_pong_at, Time.current)
+  #   end
+  # end
 
   def self.send_to(user, channel, data)
     SocketChannel.broadcast_to("socket_#{user.id}_#{channel}", data)
