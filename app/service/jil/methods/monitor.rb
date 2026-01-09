5ddb8b0cda8d30168002eb3be7aee371df4fcb6a
Rocco Nicholls <rocco11nicholls@gmail.com>
2026-01-09 12:14:43 -0700

    Monitors use channel, not id

--- app/service/jil/methods/monitor.rb
+++ app/service/jil/methods/monitor.rb
@@ -13,13 +13,13 @@ class Jil::Methods::Monitor < Jil::Methods::Base
   #   #data(Hash)
 
   def loading(name, bool)
-    ::MonitorChannel.broadcast_to(@jil.user, id: name, loading: bool)
-    { id: name }
+    ::MonitorChannel.broadcast_to(@jil.user, channel: name, loading: bool)
+    { channel: name }
   end
 
   def broadcast(name, param_blocks, loading)
     data = param_blocks.inject({}) { |acc, hash| acc.merge(hash) }.deep_symbolize_keys
-    data[:id] = name
+    data[:channel] = name
     data[:timestamp] = (
       case data[:timestamp]
       when TrueClass then ::Time.current.to_i
@@ -30,15 +30,15 @@ class Jil::Methods::Monitor < Jil::Methods::Base
     data[:loading] = loading
 
     ::MonitorChannel.broadcast_to(@jil.user, data)
-    { id: name }
+    { channel: name }
   end
 
   def refresh(name, data)
     ::Jil.trigger(
       @jil.user, :monitor,
-      { id: name, refresh: true }.reverse_merge(data.presence || {})
+      { channel: name, refresh: true }.reverse_merge(data.presence || {})
     )
-    { id: name }
+    { channel: name }
   end
 
   # [MonitorData]
