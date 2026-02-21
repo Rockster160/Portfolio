2712e0064e7b727e36e7333599180f0df91d2830
Rocco Nicholls <rocco11nicholls@gmail.com>
2026-02-21 14:59:37 -0700

    Added potty buttons

--- app/controllers/whisper_controller.rb
+++ app/controllers/whisper_controller.rb
@@ -10,8 +10,12 @@ class WhisperController < ApplicationController
       221, # Nap Toggle
       230, # Gone Toggle
       225, # Sleep
+      # 245, # Quiet - Needs more
+      208, # 1
+      209, # 2
       # NOTE! When adding new tasks, ensure they are shared!
     ]
+    # chels = User.find(58128)
     # task_ids.each { SharedTask.find_or_create_by(user: chels, task_id: _1) }
     @tasks = current_user.accessible_tasks.where(id: task_ids).sort_by { |t| task_ids.index(t.id) }
   end
