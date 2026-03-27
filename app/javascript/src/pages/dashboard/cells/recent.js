be5ba8c75aba066e225610bdc20a065fce8ed478
Rocco Nicholls <rocco11nicholls@gmail.com>
2026-03-27 15:14:23 -0600

    Beter masking and JS-cop

--- app/javascript/src/pages/dashboard/cells/recent.js
+++ app/javascript/src/pages/dashboard/cells/recent.js
@@ -1,19 +1,35 @@
-import { Text } from "../_text"
-import { Time } from "./_time"
+import { Text } from "../_text";
+import { Time } from "./_time";
 
-(function() {
-  var renderEvents = function(cell, events) {
-    cell.lines(events.map(function(item) {
-      if (cell.data.quiet && (item.name.length == 1 || item.name == "Zish" || item.name == "Z*")) { return }
+(function () {
+  var renderEvents = function (cell, events) {
+    cell.lines(
+      events
+        .map(function (item) {
+          if (
+            cell.data.quiet &&
+            (item.name.length == 1 ||
+              /^.\*$/.test(item.name) ||
+              item.name == "Zish")
+          ) {
+            return;
+          }
 
-      var timestamp = Time.at(Date.parse(item.timestamp))
-      var h = timestamp.getHours()
-      var time = (h > 12 ? h - 12 : h) + ":" + String(timestamp.getMinutes()).padStart(2, "0")
-      var notes = item.notes ? " (" + item.notes + ")" : ""
+          var timestamp = Time.at(Date.parse(item.timestamp));
+          var h = timestamp.getHours();
+          var time =
+            (h > 12 ? h - 12 : h) +
+            ":" +
+            String(timestamp.getMinutes()).padStart(2, "0");
+          var notes = item.notes ? " (" + item.notes + ")" : "";
 
-      return Text.justify(item.name + notes, time || "")
-    }).filter(function(line) { return line && line.length > 0 }))
-  }
+          return Text.justify(item.name + notes, time || "");
+        })
+        .filter(function (line) {
+          return line && line.length > 0;
+        }),
+    );
+  };
 
   Cell.register({
     title: "Recent",
@@ -22,36 +38,38 @@ import { Time } from "./_time"
       quiet: true,
     },
     commands: {
-      quiet: function() {
-        this.data.quiet = !this.data.quiet
-        this.reload()
+      quiet: function () {
+        this.data.quiet = !this.data.quiet;
+        this.reload();
       },
     },
-    socket: Server.socket("RecentEventsChannel", function(msg) {
-      if (!msg.recent_events) { return }
+    socket: Server.socket("RecentEventsChannel", function (msg) {
+      if (!msg.recent_events) {
+        return;
+      }
 
-      renderEvents(this, msg.recent_events)
+      renderEvents(this, msg.recent_events);
     }),
-    reloader: function() {
-      var cell = this
-      $.getJSON("/action_events", function(res) {
-        renderEvents(cell, res.data)
-      }).fail(function(data) {
-        cell.text("Failed to retrieve: " + JSON.stringify(data))
-      })
+    reloader: function () {
+      var cell = this;
+      $.getJSON("/action_events", function (res) {
+        renderEvents(cell, res.data);
+      }).fail(function (data) {
+        cell.text("Failed to retrieve: " + JSON.stringify(data));
+      });
     },
-    command: function(text) {
+    command: function (text) {
       if (/^\d+/.test(text)) {
-        Server.post("/functions/pullups_counter/run", { count: text })
+        Server.post("/functions/pullups_counter/run", { count: text });
       } else if (/^\s*Wordle [\d,]+.*?(\d|X)\/6/.test(text)) {
-        let num = text.match(/^\s*Wordle [\d,]+.*?(\d|X)\/6/)[1]
-        Server.post("/action_events", { name: "Wordle", notes: num })
+        let num = text.match(/^\s*Wordle [\d,]+.*?(\d|X)\/6/)[1];
+        Server.post("/action_events", { name: "Wordle", notes: num });
       } else {
-        var [name, ...notes] = text.split(" ")
-        name = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
-        notes = notes.join(" ")
-        Server.post("/action_events", { name: name, notes: notes })
+        var [name, ...notes] = text.split(" ");
+        name = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+        notes = notes.join(" ");
+        Server.post("/action_events", { name: name, notes: notes });
       }
     },
-  })
-})()
+  });
+})();
