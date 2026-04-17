4bc8d385feab2b8c457d980db1f6a0b8d06ce543
Rocco Nicholls <rocco11nicholls@gmail.com>
2026-04-17 08:55:30 -0600

    Allow MD in the quick actions page

--- public/quick_actions/md_render.js
+++ public/quick_actions/md_render.js
@@ -14,16 +14,33 @@ export let toMd = function(text) {
       return `__TOKEN${id}__`
   }).replaceAll(/([\p{So}\p{Sk}\p{Sm}\p{Sc}\p{S}\p{C}]+)/gu, (match) => {
     return emoji(match)
-  }).replaceAll(/\[ico (.*?)(( [\w-]+: .*?)*)\]/gu, (match, p1, p2) => {
+  }).replaceAll(/\[hr\]/gi, '<hr>')
+  .replaceAll(/\[bg (.*?)\](.*?)\[\/bg\]/gi, '<span style="background-color: $1;">$2</span>')
+  .replaceAll(/\[color (.*?)\](.*?)\[\/color\]/gi, '<span style="color: $1;">$2</span>')
+  .replaceAll(/\[bold\](.*?)\[\/bold\]/gi, '<b>$1</b>')
+  .replaceAll(/\[ani \"(.*?)\"\]/gi, '<textanimate steps="$1"> </textanimate>')
+  .replaceAll(/\[ico (.*?)(( [\w-]+: .*?)*)\]/gu, (match, p1, p2) => {
     return emoji(null, `ti ti-${p1}`, { style: p2 })
   }).replaceAll(/\[img (.*?)\]/gu, (match, p1) => {
     return img(p1)
-  }).replaceAll(/(\\n|\n)/g, (match) => {
+  }).replaceAll(/\[plain (.*?)\]\((.*?)\)/gi, '<a href="$2" target="_blank" rel="noopener noreferrer" class="dash-link dash-link-plain">$1</a>')
+  .replaceAll(/\[((?!plain )[^\]]*?)\]\((.*?)\)/gi, '<a href="$2" target="_blank" rel="noopener noreferrer" class="dash-link">$1</a>')
+  .replaceAll(/(\\n|\n)/g, (match) => {
     return "<br>"
   }).replaceAll(/__TOKEN(\d+)__/g, (match, id) => {
     return hold[id] || match
   })
 }
+
+setInterval(function() {
+  document.querySelectorAll("textanimate").forEach(function(ele) {
+    let steps = ele.getAttribute("steps").split("")
+    let current_step = parseInt(ele.getAttribute("step") || 0)
+    let next_step = (current_step + 1) % steps.length
+    ele.textContent = steps[next_step]
+    ele.setAttribute("step", next_step)
+  })
+}, 100)
 
 let img = function(filename) {
   if (!filename.startsWith("http") && !filename.startsWith("/")) {
