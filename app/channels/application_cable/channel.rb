ab28fee57ef1e059769486017f0738351a56a997
Rocco Nicholls <rocco11nicholls@gmail.com>
2026-04-23 12:07:44 -0600

    Show connections, fix errors.

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,5 +1,10 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    def subscribed
+      super
+      ::PrettyLogger.info("\e[35m[WS:SUB] #{self.class.name} | user=#{current_user&.username || "guest"} | params=#{params.except(:channel).to_json}\e[0m")
+    end
+
     def pretty_log_data(data)
       identifier = (
         if current_user
