a204b63a895cf682e2de04013a04890bf2676904
Rocco Nicholls <rocco11nicholls@gmail.com>
2026-08-27 17:59:10 -0600

    Updated to new IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # New person: Add their `id_rsa.pub` (or other desired key) to
 # `Portfolio:/home/deploy/.ssh/authorized_keys`
-server "165.227.15.235", user: :deploy, roles: [:app, :web, :db]
+server "142.93.240.160", user: :deploy, roles: [:app, :web, :db]
 
 set :stage,           :production
 set :rails_env,       :production
